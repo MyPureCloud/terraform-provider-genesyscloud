60942e5f7ce2161d8c7f905f92f4dc3ce1838354
Ronan Watkins <ronan.watkins@genesys.com>
2021-10-13 11:32:56 +0100

    Adding datatable datasource

---
+++ genesyscloud/data_source_genesyscloud_architect_datatable_test.go
@@ -0,0 +1,62 @@
+package genesyscloud
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDataSourceArchitectDatatable(t *testing.T) {
+	var (
+		tableResource = "arch-table1"
+		tableName     = "Terraform Table1-" + uuid.NewString()
+		tableDesc     = "Terraform test table1"
+
+		propNameKey = "key"
+		propBool    = "Test Bool"
+		typeString = "string"
+		typeBool   = "boolean"
+
+		tableDataSource = "arch-table1-ds"
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				// Create datatable with a key and one other property
+				Config: generateArchitectDatatableResource(
+					tableResource,
+					tableName,
+					strconv.Quote(tableDesc),
+					generateArchitectDatatableProperty(propBool, typeBool, nullValue, nullValue),
+					generateArchitectDatatableProperty(propNameKey, typeString, nullValue, nullValue),
+				) + generateArchitectDatatableDataSource(
+					tableDataSource,
+					"genesyscloud_architect_datatable."+tableResource+".name",
+					"genesyscloud_architect_datatable."+tableResource),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair("data.genesyscloud_architect_datatable."+tableDataSource, "id", "genesyscloud_architect_datatable."+tableResource, "id"),
+				),
+			},
+		},
+		CheckDestroy: testVerifyDatatablesDestroyed,
+	})
+}
+
+func generateArchitectDatatableDataSource(
+	resourceID string,
+	name string,
+	// Must explicitly use depends_on in terraform v0.13 when a data source references a resource
+	// Fixed in v0.14 https://github.com/hashicorp/terraform/pull/26284
+	dependsOnResource string) string {
+	return fmt.Sprintf(`data "genesyscloud_architect_datatable" "%s" {
+		name = %s
+		depends_on=[%s]
+	}
+	`, resourceID, name, dependsOnResource)
+}
