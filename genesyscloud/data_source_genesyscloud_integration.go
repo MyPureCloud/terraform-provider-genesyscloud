834c75a3c4b5301ddd8864da9a94aff5bce6e5f1
Michael Roddy <88041719+michaeljroddy@users.noreply.github.com>
2021-10-19 14:16:08 +0100

    Integration data source (#145)
    * adding integration data source
    
    * adding integration.md and data source example
    
    * adding integration name to error message

---
+++ genesyscloud/data_source_genesyscloud_integration.go
@@ -0,0 +1,55 @@
+package genesyscloud
+
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/mypurecloud/platform-client-sdk-go/v56/platformclientv2"
+	"time"
+)
+
+func dataSourceIntegration() *schema.Resource {
+	return &schema.Resource{
+		Description: "Data source for Genesys Cloud integration. Select an integration by name",
+		ReadContext: readWithPooledClient(dataSourceIntegrationRead),
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Description:      "The name of the integration",
+				Type:             schema.TypeString,
+				Required:         true,
+			},
+		},
+	}
+}
+
+func dataSourceIntegrationRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	sdkConfig := m.(*providerMeta).ClientConfig
+	integrationAPI := platformclientv2.NewIntegrationsApiWithConfig(sdkConfig)
+
+	integrationName := d.Get("name").(string)
+
+	return withRetries(ctx, 15*time.Second, func() *resource.RetryError {
+		for pageNum := 1; ; pageNum++ {
+			integrations, _, getErr := integrationAPI.GetIntegrations(100, pageNum, "", nil, "", "")
+
+			if getErr != nil {
+				return resource.NonRetryableError(fmt.Errorf("failed to get page of integrations: %s", getErr))
+			}
+
+			if integrations.Entities == nil || len(*integrations.Entities) == 0 {
+				return resource.RetryableError(fmt.Errorf("no integrations found with name: %s", integrationName))
+			}
+
+			for _, integration := range *integrations.Entities {
+				if  integration.Name != nil && *integration.Name ==  integrationName {
+					d.SetId(*integration.Id)
+					return nil
+				}
+			}
+
+		}
+	})
+
+}
