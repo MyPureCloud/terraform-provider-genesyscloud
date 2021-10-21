b2fac02a4b48f886194efe408de92c1df8f15e33
Anthony Prince Merluza <zero.bash@Gmail.com>
2021-10-21 15:36:01 +0800

    Add DID data source (#150)

---
+++ examples/data-sources/genesyscloud_telephony_providers_edges_did/data-source.tf
@@ -0,0 +1,3 @@
+data "genesyscloud_telephony_providers_edges_did" "didNumber" {
+  phone_number = "+13175550100"
+}
---
+++ genesyscloud/data_source_genesyscloud_telephony_providers_edges_did.go
@@ -0,0 +1,57 @@
+package genesyscloud
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/mypurecloud/platform-client-sdk-go/v56/platformclientv2"
+)
+
+func dataSourceDid() *schema.Resource {
+	return &schema.Resource{
+		Description: "Data source for Genesys Cloud DID. The identifier is the E-164 phone number.",
+		ReadContext: readWithPooledClient(dataSourceDidRead),
+		Schema: map[string]*schema.Schema{
+			"phone_number": {
+				Description:      "Phone number for the DID.",
+				Type:             schema.TypeString,
+				Required:         true,
+				ValidateDiagFunc: validatePhoneNumber,
+			},
+		},
+	}
+}
+
+func dataSourceDidRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	sdkConfig := m.(*providerMeta).ClientConfig
+	telephonyAPI := platformclientv2.NewTelephonyProvidersEdgeApiWithConfig(sdkConfig)
+
+	didPhoneNumber := d.Get("phone_number").(string)
+
+	return withRetries(ctx, 15*time.Second, func() *resource.RetryError {
+		for pageNum := 1; ; pageNum++ {
+			dids, _, getErr := telephonyAPI.GetTelephonyProvidersEdgesDids(100, pageNum, "", "", didPhoneNumber, "", "", nil)
+
+			if getErr != nil {
+				return resource.NonRetryableError(fmt.Errorf("error requesting list of DIDs: %s", getErr))
+			}
+
+			if dids.Entities == nil || len(*dids.Entities) == 0 {
+				return resource.RetryableError(fmt.Errorf("no DIDs found"))
+			}
+
+			for _, did := range *dids.Entities {
+				if *did.PhoneNumber == didPhoneNumber {
+					d.SetId(*did.Id)
+					return nil
+				}
+			}
+
+		}
+	})
+
+}
