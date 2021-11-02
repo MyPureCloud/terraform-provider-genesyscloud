86e93bc7940c4e7e154c59e52003fcc22033189e
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2021-11-02 16:05:30 +0800

    Add data source and docs

--- genesyscloud/provider.go
+++ genesyscloud/provider.go
@@ -124,6 +124,7 @@ func New(version string) func() *schema.Provider {
 				"genesyscloud_integration_credential":                      dataSourceIntegrationCredential(),
 				"genesyscloud_location":                                    dataSourceLocation(),
 				"genesyscloud_oauth_client":                                dataSourceOAuthClient(),
+				"genesyscloud_quality_forms_evaluation":                    dataSourceQualityFormsEvaluations(),
 				"genesyscloud_routing_language":                            dataSourceRoutingLanguage(),
 				"genesyscloud_routing_queue":                               dataSourceRoutingQueue(),
 				"genesyscloud_routing_skill":                               dataSourceRoutingSkill(),
