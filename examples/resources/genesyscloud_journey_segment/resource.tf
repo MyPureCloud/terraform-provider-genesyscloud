1567600b54c7b80a6fce17c308a87be0f0e3cfc5
istvan_bihari <istvan.bihari@genesys.com>
2022-06-20 11:17:33 +0200

    BLD-56635: Add initial documentation

--- examples/resources/genesyscloud_journey_segment/resource.tf
+++ examples/resources/genesyscloud_journey_segment/resource.tf
@@ -1,6 +1,6 @@
 resource "genesyscloud_journey_segment" "test_journey_segment" {
   display_name = "journey_segment_1"
-  description = "Description of Journey Segment"
-  color = "008000"
-  scope = "Customer"
+  description  = "Description of Journey Segment"
+  color        = "008000"
+  scope        = "Customer"
 }
