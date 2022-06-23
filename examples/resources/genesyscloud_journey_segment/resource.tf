c090c68cc1a2c002c80866b67f15cbddf3fa5e89
gweszelovszki <gabor.weszelovszki@genesys.com>
2022-06-23 11:26:57 +0200

    BLD-56635: update example journey segment

--- examples/resources/genesyscloud_journey_segment/resource.tf
+++ examples/resources/genesyscloud_journey_segment/resource.tf
@@ -1,6 +1,31 @@
-resource "genesyscloud_journey_segment" "test_journey_segment" {
-  display_name = "journey_segment_1"
-  description  = "Description of Journey Segment"
-  color        = "#008000"
-  scope        = "Customer"
+resource "genesyscloud_journey_segment" "example_journey_segment" {
+  display_name            = "journey_segment_1"
+  color                   = "#008000"
+  scope                   = "Session"
+  should_display_to_agent = false
+  context {
+    patterns {
+      criteria {
+        key                = "geolocation.postalCode"
+        values             = ["something"]
+        operator           = "equal"
+        should_ignore_case = true
+        entity_type        = "visit"
+      }
+    }
+  }
+  journey {
+    patterns {
+      criteria {
+        key                = "page.hostname"
+        values             = ["something_else"]
+        operator           = "equal"
+        should_ignore_case = false
+      }
+      count        = 1
+      stream_type  = "Web"
+      session_type = "web"
+      event_name   = "EventName"
+    }
+  }
 }
