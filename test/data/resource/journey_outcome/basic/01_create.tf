1fde6e9e594dd33c520cb97078c5608c3823cdbe
gweszelovszki <gabor.weszelovszki@genesys.com>
2022-06-27 16:59:39 +0200

    BLD-56637: extend test

--- test/data/resource/journey_outcome/basic/01_create.tf
+++ test/data/resource/journey_outcome/basic/01_create.tf
@@ -1,3 +1,23 @@
 resource "genesyscloud_journey_outcome" "terraform_test_-TEST-CASE-" {
-  display_name            = "terraform_test_-TEST-CASE-"
+  is_active     = true
+  display_name = "terraform_test_-TEST-CASE-"
+  description  = "test description of journey outcome"
+  is_positive   = true
+  journey {
+    patterns {
+      criteria {
+        key                = "attributes.bleki.value"
+        values             = ["Blabla"]
+        operator           = "notEqual"
+        should_ignore_case = true
+      }
+      count        = 1
+      stream_type  = "Web"
+      session_type = "web"
+    }
+  }
+  associated_value_field {
+    data_type = "Number"
+    name      = "attributes.cartValue.value"
+  }
 }
--- test/data/resource/journey_outcome/basic/02_update.tf
+++ test/data/resource/journey_outcome/basic/02_update.tf
@@ -1,3 +1,21 @@
 resource "genesyscloud_journey_outcome" "terraform_test_-TEST-CASE-" {
-  display_name            = "terraform_test_-TEST-CASE-_updated"
+  is_active     = false
+  display_name = "terraform_test_-TEST-CASE-_updated"
+  description  = "updated test description of journey outcome"
+  is_positive   = false
+  context {
+    patterns {
+      criteria {
+        key                = "geolocation.postalCode"
+        values             = ["something"]
+        operator           = "equal"
+        should_ignore_case = true
+        entity_type        = "visit"
+      }
+    }
+  }
+  associated_value_field {
+    data_type = "Integer"
+    name      = "updated associated value field name"
+  }
 }
