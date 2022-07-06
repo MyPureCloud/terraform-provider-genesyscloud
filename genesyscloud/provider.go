fa141d2e40f5656d1e5bf07b3dc94adf235602e5
Charlie Conneely <charlie.conneely@genesys.com>
2022-07-06 12:15:24 +0100

    Created an outbound attempt limit resource + tests

--- genesyscloud/provider.go
+++ genesyscloud/provider.go
@@ -101,6 +101,7 @@ func New(version string) func() *schema.Provider {
 				"genesyscloud_location":                                    resourceLocation(),
 				"genesyscloud_recording_media_retention_policy":            resourceMediaRetentionPolicy(),
 				"genesyscloud_oauth_client":                                resourceOAuthClient(),
+				"genesyscloud_outbound_attempt_limit":                      resourceOutboundAttemptLimit(),
 				"genesyscloud_processautomation_trigger":                   resourceProcessAutomationTrigger(),
 				"genesyscloud_quality_forms_evaluation":                    resourceEvaluationForm(),
 				"genesyscloud_quality_forms_survey":                        resourceSurveyForm(),
