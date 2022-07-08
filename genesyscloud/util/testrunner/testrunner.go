26b3cf618e7360f1deeaba0a5f2066f1b1293e96
istvan_bihari <istvan.bihari@genesys.com>
2022-07-08 15:34:23 +0200

    BLD-56636: Hide TestObjectIdPrefix when possible

--- genesyscloud/util/testrunner/testrunner.go
+++ genesyscloud/util/testrunner/testrunner.go
@@ -10,9 +10,10 @@ import (
 )
 
 const (
-	dataSourceTestType = "data_source"
-	resourceTestType   = "resource"
-	TestObjectIdPrefix = "terraform_test_"
+	dataSourceTestType            = "data_source"
+	resourceTestType              = "resource"
+	TestObjectIdPrefix            = "terraform_test_"
+	testObjectTestCasePlaceHolder = "-TEST-CASE-"
 )
 
 func GetTestDataPath(elem ...string) string {
@@ -39,7 +40,7 @@ func GenerateTestSteps(testType string, resourceName string, testCaseName string
 		if !testCaseDirEntry.IsDir() && strings.HasSuffix(testCaseDirEntry.Name(), ".tf") {
 			testCaseStepFilePath := filepath.Join(testCasePath, testCaseDirEntry.Name())
 			testCaseResource, _ := os.ReadFile(testCaseStepFilePath)
-			config := strings.ReplaceAll(string(testCaseResource), "-TEST-CASE-", testCaseName)
+			config := strings.ReplaceAll(string(testCaseResource), testObjectTestCasePlaceHolder, testCaseName)
 			var checkFunc resource.TestCheckFunc = nil
 			if checkFuncs != nil && checkFuncIndex < len(checkFuncs) {
 				checkFunc = checkFuncs[checkFuncIndex]
