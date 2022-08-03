b8752ef279b110f46f920b6d87890afd1b598eea
Charlie Conneely <charlie.conneely@genesys.com>
2022-08-03 13:02:49 +0100

    Created outbound contact list filter resource + tests

--- genesyscloud/provider.go
+++ genesyscloud/provider.go
@@ -105,6 +105,7 @@ func New(version string) func() *schema.Provider {
 				"genesyscloud_recording_media_retention_policy":            resourceMediaRetentionPolicy(),
 				"genesyscloud_oauth_client":                                resourceOAuthClient(),
 				"genesyscloud_outbound_attempt_limit":                      resourceOutboundAttemptLimit(),
+				"genesyscloud_outbound_contactlistfilter":                  resourceOutboundContactListFilter(),
 				"genesyscloud_processautomation_trigger":                   resourceProcessAutomationTrigger(),
 				"genesyscloud_quality_forms_evaluation":                    resourceEvaluationForm(),
 				"genesyscloud_quality_forms_survey":                        resourceSurveyForm(),
