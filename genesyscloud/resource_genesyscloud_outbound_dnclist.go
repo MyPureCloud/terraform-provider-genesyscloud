a15d8e94ebfefe5502b573042a3d82dc3c345825
Charlie Conneely <charlie.conneely@genesys.com>
2022-08-29 16:19:41 +0100

    Fixing merge conflicts

---
+++ genesyscloud/resource_genesyscloud_outbound_dnclist.go
@@ -0,0 +1,272 @@
+package genesyscloud
+
+import (
+	"context"
+	"fmt"
+	"github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/consistency_checker"
+	"log"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/mypurecloud/platform-client-sdk-go/v75/platformclientv2"
+)
+
+func getAllOutboundDncLists(_ context.Context, clientConfig *platformclientv2.Configuration) (ResourceIDMetaMap, diag.Diagnostics) {
+	resources := make(ResourceIDMetaMap)
+	outboundAPI := platformclientv2.NewOutboundApiWithConfig(clientConfig)
+
+	for pageNum := 1; ; pageNum++ {
+		const pageSize = 100
+		dncListConfigs, _, getErr := outboundAPI.GetOutboundDnclists(false, false, pageSize, pageNum, true, "", "", "", []string{}, "", "")
+		if getErr != nil {
+			return nil, diag.Errorf("Failed to get page of dnc list configs: %v", getErr)
+		}
+		if dncListConfigs.Entities == nil || len(*dncListConfigs.Entities) == 0 {
+			break
+		}
+		for _, dncListConfig := range *dncListConfigs.Entities {
+			resources[*dncListConfig.Id] = &ResourceMeta{Name: *dncListConfig.Name}
+		}
+	}
+
+	return resources, nil
+}
+
+func outboundDncListExporter() *ResourceExporter {
+	return &ResourceExporter{
+		GetResourcesFunc: getAllWithPooledClient(getAllOutboundDncLists),
+		RefAttrs: map[string]*RefAttrSettings{
+			"division_id": {RefType: "genesyscloud_auth_division"},
+		},
+	}
+}
+
+func resourceOutboundDncList() *schema.Resource {
+	return &schema.Resource{
+		Description: `Genesys Cloud Outbound DNC List`,
+
+		CreateContext: createWithPooledClient(createOutboundDncList),
+		ReadContext:   readWithPooledClient(readOutboundDncList),
+		UpdateContext: updateWithPooledClient(updateOutboundDncList),
+		DeleteContext: deleteWithPooledClient(deleteOutboundDncList),
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		SchemaVersion: 1,
+		Schema: map[string]*schema.Schema{
+			`name`: {
+				Description: `The name of the DncList.`,
+				Required:    true,
+				Type:        schema.TypeString,
+			},
+			`contact_method`: {
+				Description:  `The contact method. Required if dncSourceType is rds.`,
+				Optional:     true,
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringInSlice([]string{`Email`, `Phone`}, false),
+			},
+			`login_id`: {
+				Description: `A dnc.com loginId. Required if the dncSourceType is dnc.com.`,
+				Optional:    true,
+				Type:        schema.TypeString,
+			},
+			`dnc_codes`: {
+				Description: `The list of dnc.com codes to be treated as DNC. Required if the dncSourceType is dnc.com.`,
+				Optional:    true,
+				Type:        schema.TypeList,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+			},
+			`license_id`: {
+				Description: `A gryphon license number. Required if the dncSourceType is gryphon.`,
+				Optional:    true,
+				Type:        schema.TypeString,
+			},
+			`division_id`: {
+				Description: `The division this DncList belongs to.`,
+				Optional:    true,
+				Computed:    true,
+				Type:        schema.TypeString,
+			},
+			`dnc_source_type`: {
+				Description:  `The type of the DncList.`,
+				Required:     true,
+				ForceNew:     true,
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringInSlice([]string{`rds`, `dnc.com`, `gryphon`}, false),
+			},
+		},
+	}
+}
+
+func createOutboundDncList(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	name := d.Get("name").(string)
+	contactMethod := d.Get("contact_method").(string)
+	loginId := d.Get("login_id").(string)
+	dncCodes := interfaceListToStrings(d.Get("dnc_codes").([]interface{}))
+	licenseId := d.Get("license_id").(string)
+	dncSourceType := d.Get("dnc_source_type").(string)
+
+	sdkConfig := meta.(*providerMeta).ClientConfig
+	outboundApi := platformclientv2.NewOutboundApiWithConfig(sdkConfig)
+
+	sdkDncListCreate := platformclientv2.Dnclistcreate{
+		DncCodes: &dncCodes,
+		Division: buildSdkDomainEntityRef(d, "division_id"),
+	}
+
+	if name != "" {
+		sdkDncListCreate.Name = &name
+	}
+	if contactMethod != "" {
+		sdkDncListCreate.ContactMethod = &contactMethod
+	}
+	if loginId != "" {
+		sdkDncListCreate.LoginId = &loginId
+	}
+	if licenseId != "" {
+		sdkDncListCreate.LicenseId = &licenseId
+	}
+	if dncSourceType != "" {
+		sdkDncListCreate.DncSourceType = &dncSourceType
+	}
+
+	log.Printf("Creating Outbound DNC list %s", name)
+	outboundDncList, _, err := outboundApi.PostOutboundDnclists(sdkDncListCreate)
+	if err != nil {
+		return diag.Errorf("Failed to create Outbound DNC list %s: %s", name, err)
+	}
+
+	d.SetId(*outboundDncList.Id)
+
+	log.Printf("Created Outbound DNC list %s %s", name, *outboundDncList.Id)
+	return readOutboundDncList(ctx, d, meta)
+}
+
+func updateOutboundDncList(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	name := d.Get("name").(string)
+	contactMethod := d.Get("contact_method").(string)
+	loginId := d.Get("login_id").(string)
+	dncCodes := interfaceListToStrings(d.Get("dnc_codes").([]interface{}))
+	licenseId := d.Get("license_id").(string)
+
+	sdkConfig := meta.(*providerMeta).ClientConfig
+	outboundApi := platformclientv2.NewOutboundApiWithConfig(sdkConfig)
+
+	sdkDncList := platformclientv2.Dnclist{
+		DncCodes: &dncCodes,
+		Division: buildSdkDomainEntityRef(d, "division_id"),
+	}
+
+	if name != "" {
+		sdkDncList.Name = &name
+	}
+	if contactMethod != "" {
+		sdkDncList.ContactMethod = &contactMethod
+	}
+	if loginId != "" {
+		sdkDncList.LoginId = &loginId
+	}
+	if licenseId != "" {
+		sdkDncList.LicenseId = &licenseId
+	}
+
+	log.Printf("Updating Outbound DNC list %s", name)
+	diagErr := retryWhen(isVersionMismatch, func() (*platformclientv2.APIResponse, diag.Diagnostics) {
+		// Get current Outbound DNC list version
+		outboundDncList, resp, getErr := outboundApi.GetOutboundDnclist(d.Id(), false, false)
+		if getErr != nil {
+			return resp, diag.Errorf("Failed to read Outbound DNC list %s: %s", d.Id(), getErr)
+		}
+		sdkDncList.Version = outboundDncList.Version
+		outboundDncList, _, updateErr := outboundApi.PutOutboundDnclist(d.Id(), sdkDncList)
+		if updateErr != nil {
+			return resp, diag.Errorf("Failed to update Outbound DNC list %s: %s", name, updateErr)
+		}
+		return nil, nil
+	})
+	if diagErr != nil {
+		return diagErr
+	}
+
+	log.Printf("Updated Outbound DNC list %s", name)
+	return readOutboundDncList(ctx, d, meta)
+}
+
+func readOutboundDncList(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	sdkConfig := meta.(*providerMeta).ClientConfig
+	outboundApi := platformclientv2.NewOutboundApiWithConfig(sdkConfig)
+
+	log.Printf("Reading Outbound DNC list %s", d.Id())
+
+	return withRetriesForRead(ctx, d, func() *resource.RetryError {
+		sdkDncList, resp, getErr := outboundApi.GetOutboundDnclist(d.Id(), false, false)
+		if getErr != nil {
+			if isStatus404(resp) {
+				return resource.RetryableError(fmt.Errorf("failed to read Outbound DNC list %s: %s", d.Id(), getErr))
+			}
+			return resource.NonRetryableError(fmt.Errorf("failed to read Outbound DNC list %s: %s", d.Id(), getErr))
+		}
+
+		cc := consistency_checker.NewConsistencyCheck(ctx, d, meta, resourceOutboundDncList())
+
+		if sdkDncList.Name != nil {
+			_ = d.Set("name", *sdkDncList.Name)
+		}
+		if sdkDncList.ContactMethod != nil {
+			_ = d.Set("contact_method", *sdkDncList.ContactMethod)
+		}
+		if sdkDncList.LoginId != nil {
+			_ = d.Set("login_id", *sdkDncList.LoginId)
+		}
+		if sdkDncList.DncCodes != nil {
+			var dncCodes []string
+			for _, code := range *sdkDncList.DncCodes {
+				dncCodes = append(dncCodes, code)
+			}
+			_ = d.Set("dnc_codes", dncCodes)
+		}
+		if sdkDncList.LicenseId != nil {
+			_ = d.Set("license_id", *sdkDncList.LicenseId)
+		}
+		if sdkDncList.Division != nil && sdkDncList.Division.Id != nil {
+			_ = d.Set("division_id", *sdkDncList.Division.Id)
+		}
+
+		log.Printf("Read Outbound DNC list %s %s", d.Id(), *sdkDncList.Name)
+		return cc.CheckState()
+	})
+}
+
+func deleteOutboundDncList(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	sdkConfig := meta.(*providerMeta).ClientConfig
+	outboundApi := platformclientv2.NewOutboundApiWithConfig(sdkConfig)
+
+	diagErr := retryWhen(isStatus400, func() (*platformclientv2.APIResponse, diag.Diagnostics) {
+		log.Printf("Deleting Outbound DNC list")
+		resp, err := outboundApi.DeleteOutboundDnclist(d.Id())
+		if err != nil {
+			return resp, diag.Errorf("Failed to delete Outbound DNC list: %s", err)
+		}
+		return resp, nil
+	})
+	if diagErr != nil {
+		return diagErr
+	}
+
+	return withRetries(ctx, 30*time.Second, func() *resource.RetryError {
+		_, resp, err := outboundApi.GetOutboundDnclist(d.Id(), false, false)
+		if err != nil {
+			if isStatus404(resp) {
+				// Outbound DNC list deleted
+				log.Printf("Deleted Outbound DNC list %s", d.Id())
+				return nil
+			}
+			return resource.NonRetryableError(fmt.Errorf("error deleting Outbound DNC list %s: %s", d.Id(), err))
+		}
+
+		return resource.RetryableError(fmt.Errorf("Outbound DNC list %s still exists", d.Id()))
+	})
+}
---
+++ genesyscloud/resource_genesyscloud_outbound_dnclist_test.go
@@ -0,0 +1,193 @@
+package genesyscloud
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/mypurecloud/platform-client-sdk-go/v75/platformclientv2"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+const (
+	dncLoginId     = "96CAAC02650543056DF1ADA796A0082ED152561EDEE1"
+	gryphonLicense = "4ADA-9A3B-5DAD-4FAD-95A7-4C31-425B-8594"
+)
+
+func TestAccResourceOutboundDncList(t *testing.T) {
+	t.Parallel()
+	var (
+		resourceID    = "dnc_list"
+		name          = "Test DNC List " + uuid.NewString()
+		dncSourceType = "rds"
+		contactMethod = "Phone"
+		dncCode1      = "B"
+		dncCode2      = "D"
+		dncCode3      = "P"
+		dncCode4      = "S"
+		dncCodes      = []string{strconv.Quote(dncCode1), strconv.Quote(dncCode2)}
+
+		nameUpdated          = "Test DNC List " + uuid.NewString()
+		dncSourceTypeUpdate1 = "dnc.com"
+		dncCodesUpdated      = append(dncCodes, strconv.Quote(dncCode3), strconv.Quote(dncCode4))
+
+		dncSourceTypeUpdate2 = "gryphon"
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: generateOutboundDncList(
+					resourceID,
+					name,
+					dncSourceType,
+					contactMethod,
+					"",
+					"",
+					[]string{},
+				),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "name", name),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "dnc_source_type", dncSourceType),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "contact_method", contactMethod),
+					testDefaultHomeDivision("genesyscloud_outbound_dnclist."+resourceID),
+				),
+			},
+			{
+				// Update
+				Config: generateOutboundDncList(
+					resourceID,
+					nameUpdated,
+					dncSourceTypeUpdate1,
+					"",
+					dncLoginId,
+					"",
+					dncCodes,
+				),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "name", nameUpdated),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "dnc_source_type", dncSourceTypeUpdate1),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "login_id", dncLoginId),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "dnc_codes.0", dncCode1),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "dnc_codes.1", dncCode2),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "dnc_codes.#", fmt.Sprintf("%v", len(dncCodes))),
+					testDefaultHomeDivision("genesyscloud_outbound_dnclist."+resourceID),
+				),
+			},
+			{
+				// Update dnc codes array
+				Config: generateOutboundDncList(
+					resourceID,
+					nameUpdated,
+					dncSourceTypeUpdate1,
+					"",
+					dncLoginId,
+					"",
+					dncCodesUpdated,
+				),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "name", nameUpdated),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "dnc_source_type", dncSourceTypeUpdate1),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "login_id", dncLoginId),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "dnc_codes.0", dncCode1),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "dnc_codes.1", dncCode2),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "dnc_codes.2", dncCode3),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "dnc_codes.3", dncCode4),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "dnc_codes.#", fmt.Sprintf("%v", len(dncCodesUpdated))),
+					testDefaultHomeDivision("genesyscloud_outbound_dnclist."+resourceID),
+				),
+			},
+			{
+				// Update to dnc type Gryphon
+				Config: generateOutboundDncList(
+					resourceID,
+					nameUpdated,
+					dncSourceTypeUpdate2,
+					"",
+					"",
+					gryphonLicense,
+					[]string{},
+				),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "name", nameUpdated),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "dnc_source_type", dncSourceTypeUpdate2),
+					resource.TestCheckResourceAttr("genesyscloud_outbound_dnclist."+resourceID, "license_id", gryphonLicense),
+					testDefaultHomeDivision("genesyscloud_outbound_dnclist."+resourceID),
+				),
+			},
+			{
+				// Import/Read
+				ResourceName:            "genesyscloud_outbound_dnclist." + resourceID,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"dnc_source_type", "contact_method", "dnc_codes"},
+			},
+		},
+		CheckDestroy: testVerifyDncListDestroyed,
+	})
+}
+
+func generateOutboundDncList(
+	resourceId string,
+	name string,
+	dncSourceType string,
+	contactMethod string,
+	loginId string,
+	licenseId string,
+	dncCodes []string) string {
+	if contactMethod != "" {
+		contactMethod = fmt.Sprintf(`contact_method = "%s"`, contactMethod)
+	}
+	if loginId != "" {
+		loginId = fmt.Sprintf(`login_id = "%s"`, loginId)
+	}
+	if licenseId != "" {
+		licenseId = fmt.Sprintf(`license_id = "%s"`, licenseId)
+	}
+	return fmt.Sprintf(`
+resource "genesyscloud_outbound_dnclist" "%s" {
+	name            = "%s"
+	dnc_source_type = "%s"
+	%s
+	%s
+	%s
+	dnc_codes = [%s]
+}
+`, resourceId, name, dncSourceType, contactMethod, loginId, licenseId, strings.Join(dncCodes, ", "))
+}
+
+func generateOutboundDncListBasic(resourceId string, name string) string {
+	return fmt.Sprintf(`
+resource "genesyscloud_outbound_dnclist" "%s" {
+	name            = "%s"
+	dnc_source_type = "rds"	
+	contact_method  = "Phone"
+}
+`, resourceId, name)
+}
+
+func testVerifyDncListDestroyed(state *terraform.State) error {
+	outboundAPI := platformclientv2.NewOutboundApi()
+	for _, rs := range state.RootModule().Resources {
+		if rs.Type != "genesyscloud_outbound_dnclist" {
+			continue
+		}
+
+		dncList, resp, err := outboundAPI.GetOutboundDnclist(rs.Primary.ID, false, false)
+		if dncList != nil {
+			return fmt.Errorf("dnc list (%s) still exists", rs.Primary.ID)
+		} else if isStatus404(resp) {
+			// dnc list not found as expected
+			continue
+		} else {
+			// Unexpected error
+			return fmt.Errorf("unexpected error: %s", err)
+		}
+	}
+	// Success. All dnc lists destroyed
+	return nil
+}
