22b107ae9db689d1ebe676bb77881a9bbe07fee9
Jacob Shaw <jacob.shaw@genesys.com>
2022-09-01 14:27:31 -0400

    generated docs

--- examples/resources/genesyscloud_knowledge_category/resource.tf
+++ examples/resources/genesyscloud_knowledge_category/resource.tf
@@ -1,9 +1,9 @@
 resource "genesyscloud_knowledge_category" "example_category" {
-    knowledge_base_id = genesyscloud_knowledge.example_knowledgebase.id
-    language_code = "en-US"
-    knowledge_category {
-        name = "ExampleCategory"
-        description = "An example category"
-        parent_id = genesyscloud_knowledge_category.parent_category.id
-    }
+  knowledge_base_id = genesyscloud_knowledge.example_knowledgebase.id
+  language_code     = "en-US"
+  knowledge_category {
+    name        = "ExampleCategory"
+    description = "An example category"
+    parent_id   = genesyscloud_knowledge_category.parent_category.id
+  }
 }
--- examples/resources/genesyscloud_knowledge_knowledgebase/resource.tf
+++ examples/resources/genesyscloud_knowledge_knowledgebase/resource.tf
@@ -1,5 +1,5 @@
 resource "genesyscloud_knowledge_knowledgebase" "example_knowledgebase" {
-    name = "MyKnowledgeBase"
-    description = "An example knowledge base"
-    core_language = "en-US"
+  name          = "MyKnowledgeBase"
+  description   = "An example knowledge base"
+  core_language = "en-US"
 }
