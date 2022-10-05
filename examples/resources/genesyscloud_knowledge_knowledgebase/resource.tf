730b93ec8bb18717dce270c9b714f3d400640f21
Jacob Shaw <80964224+diemenschmaschine92@users.noreply.github.com>
2022-10-05 16:34:03 -0400

    DEVENGAGE-1049: Knowledge base resources (#345)
    * added knowledge base resource and data source with tests
    
    * added document, category, examples
    
    * generated docs
    
    * pull from main and updated imports
    
    * addressed most comments
    
    * added attribute checks
    
    * generated docs
    
    * idk
    
    * test w/o parallelization
    
    * added sleep
    
    * removed sleep
    
    * updated data source to pass whether published or unpublished
    
    Co-authored-by: Jacob Shaw <jacob.shaw@genesys.com>

---
+++ examples/resources/genesyscloud_knowledge_knowledgebase/resource.tf
@@ -0,0 +1,5 @@
+resource "genesyscloud_knowledge_knowledgebase" "example_knowledgebase" {
+  name          = "MyKnowledgeBase"
+  description   = "An example knowledge base"
+  core_language = "en-US"
+}
---
+++ genesyscloud/data_source_genesyscloud_knowledge_knowledgebase.go
@@ -0,0 +1,80 @@
+package genesyscloud
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/mypurecloud/platform-client-sdk-go/v80/platformclientv2"
+)
+
+func dataSourceKnowledgeKnowledgebase() *schema.Resource {
+	return &schema.Resource{
+		Description: "Data source for Genesys Cloud Knowledge Base. Select a knowledge base by name.",
+		ReadContext: readWithPooledClient(dataSourceKnowledgeKnowledgebaseRead),
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Description: "Knowledge base name",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"core_language": {
+				Description:  "Core language for knowledge base in which initial content must be created, language codes [en-US, en-UK, en-AU, de-DE] are supported currently, however the new DX knowledge will support all these language codes",
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringInSlice([]string{"en-US", "en-UK", "en-AU", "de-DE", "es-US", "es-ES", "fr-FR", "pt-BR", "nl-NL", "it-IT", "fr-CA"}, false),
+			},
+		},
+	}
+}
+
+func dataSourceKnowledgeKnowledgebaseRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	sdkConfig := m.(*providerMeta).ClientConfig
+	knowledgeAPI := platformclientv2.NewKnowledgeApiWithConfig(sdkConfig)
+
+	name := d.Get("name").(string)
+	coreLanguage := d.Get("core_language").(string)
+
+	// Find first non-deleted knowledge base by name. Retry in case new knowledge base is not yet indexed by search
+	return withRetries(ctx, 15*time.Second, func() *resource.RetryError {
+		for pageNum := 1; ; pageNum++ {
+			const pageSize = 100
+			publishedKnowledgeBases, _, getPublishedErr := knowledgeAPI.GetKnowledgeKnowledgebases("", "", "", fmt.Sprintf("%v", pageSize), name, coreLanguage, true, "", "")
+			unpublishedKnowledgeBases, _, getUnpublishedErr := knowledgeAPI.GetKnowledgeKnowledgebases("", "", "", fmt.Sprintf("%v", pageSize), name, coreLanguage, false, "", "")
+
+			if getPublishedErr != nil {
+				return resource.NonRetryableError(fmt.Errorf("error requesting knowledge base %s: %s", name, getPublishedErr))
+			}
+			if getUnpublishedErr != nil {
+				return resource.NonRetryableError(fmt.Errorf("error requesting knowledge base %s: %s", name, getPublishedErr))
+			}
+
+			noPublishedEntities := publishedKnowledgeBases.Entities == nil || len(*publishedKnowledgeBases.Entities) == 0
+			noUnpublishedEntities := unpublishedKnowledgeBases.Entities == nil || len(*unpublishedKnowledgeBases.Entities) == 0
+			if noPublishedEntities && noUnpublishedEntities {
+				return resource.RetryableError(fmt.Errorf("no knowledge bases found with name %s", name))
+			}
+
+			// prefer published knowledge base
+			for _, knowledgeBase := range *publishedKnowledgeBases.Entities {
+				if knowledgeBase.Name != nil && *knowledgeBase.Name == name &&
+					*knowledgeBase.CoreLanguage == coreLanguage {
+					d.SetId(*knowledgeBase.Id)
+					return nil
+				}
+			}
+			// use unpublished knowledge base if unpublished doesn't exist
+			for _, knowledgeBase := range *unpublishedKnowledgeBases.Entities {
+				if knowledgeBase.Name != nil && *knowledgeBase.Name == name &&
+					*knowledgeBase.CoreLanguage == coreLanguage {
+					d.SetId(*knowledgeBase.Id)
+					return nil
+				}
+			}
+		}
+	})
+}
