219affda0340703e75a88a4bc894797448144ece
BrendanTJones <brendan.jones@genesys.com>
2023-02-08 14:40:31 -0500

    Add direct routing object to queue

--- genesyscloud/resource_genesyscloud_routing_queue_test.go
+++ genesyscloud/resource_genesyscloud_routing_queue_test.go
@@ -490,6 +490,89 @@ func TestAccResourceRoutingQueueWrapupCodes(t *testing.T) {
 	})
 }
 
+func TestAccResourceRoutingQueueDirectRouting(t *testing.T) {
+	var (
+		queueResource1       = "test-queue-direct"
+		queueResource2       = "test-queue"
+		queueName1           = "Genesys System Direct Routing Queue"
+		queueName2           = "Terraform Test Queue2-" + uuid.NewString()
+		queueName3           = "Terraform Test Queue3-" + uuid.NewString()
+		inboundCallFlowId    = uuid.NewString()
+		voicemailFlowId      = uuid.NewString()
+		inboundEmailFlowId   = uuid.NewString()
+		inboundMessageFlowId = uuid.NewString()
+		agentWaitSeconds1    = "200"
+		waitForAgent1		 = "true"
+		agentWaitSeconds2    = "300"
+		waitForAgent2		 = "false"
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				// Create
+				Config: generateRoutingQueueResourceBasic(queueResource2, queueName2) + 
+				generateRoutingQueueResourceBasicWithDepends(
+					queueResource1,
+					"genesyscloud_routing_queue."+queueResource2,
+					queueName1,
+					generateDirectRouting(
+						agentWaitSeconds1, 	  // agentWaitSeconds
+						waitForAgent1, 	  	  // waitForAgent
+						"true", 	      	  // callEnabled
+						inboundCallFlowId, 	  // inboundCallFlowId
+						voicemailFlowId, 	  // voicemailFlowId
+						"true", 			  // emailEnabled
+						inboundEmailFlowId,   // inboundEmailFlowId
+						"true", 			  // messageEnabled
+						inboundMessageFlowId, // inboundMessageFlowId
+						"backup_queue_id = genesyscloud_routing_queue."+queueResource2+".id",
+					),
+				),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("genesyscloud_routing_queue."+queueResource1, "name", queueName1),
+					validateDirectRouting(queueResource1, agentWaitSeconds1, waitForAgent1, "true", inboundCallFlowId, voicemailFlowId, "true", inboundEmailFlowId, "true", inboundMessageFlowId),
+					resource.TestCheckResourceAttrPair("genesyscloud_routing_queue."+queueResource1, "direct_routing.0.backup_queue_id", "genesyscloud_routing_queue."+queueResource2, "id"),
+				),
+			},
+			{
+				// Update
+				Config: generateRoutingQueueResourceBasic(queueResource2, queueName3) + 
+				generateRoutingQueueResourceBasicWithDepends(
+					queueResource1,
+					"genesyscloud_routing_queue."+queueResource2,
+					queueName1,
+					generateDirectRouting(
+						agentWaitSeconds2, 	  // agentWaitSeconds
+						waitForAgent2, 	  	  // waitForAgent
+						"true", 	      	  // callEnabled
+						inboundCallFlowId, 	  // inboundCallFlowId
+						voicemailFlowId, 	  // voicemailFlowId
+						"true", 			  // emailEnabled
+						inboundEmailFlowId,   // inboundEmailFlowId
+						"true", 			  // messageEnabled
+						inboundMessageFlowId, // inboundMessageFlowId
+						"backup_queue_id = genesyscloud_routing_queue."+queueResource2+".id",
+					),
+				),
+				Check: resource.ComposeTestCheckFunc(
+					validateDirectRouting(queueResource1, agentWaitSeconds2, waitForAgent2, "true", inboundCallFlowId, voicemailFlowId, "true", inboundEmailFlowId, "true", inboundMessageFlowId),
+					resource.TestCheckResourceAttrPair("genesyscloud_routing_queue."+queueResource1, "direct_routing.0.backup_queue_id", "genesyscloud_routing_queue."+queueResource2, "id"),
+				),
+			},
+			{
+				// Import/Read
+				ResourceName:      "genesyscloud_routing_queue." + queueResource1,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+		CheckDestroy: testVerifyQueuesDestroyed,
+	})
+}
+
 func testVerifyQueuesDestroyed(state *terraform.State) error {
 	routingAPI := platformclientv2.NewRoutingApi()
 	for _, rs := range state.RootModule().Resources {
@@ -636,6 +719,42 @@ func generateQueueWrapupCodes(wrapupCodes ...string) string {
 	`, strings.Join(wrapupCodes, ", "))
 }
 
+func generateDirectRouting(
+	agentWaitSeconds string, 
+	waitForAgent string, 
+	callEnabled string,
+	callInboundFlowId string,
+	voicemailFlowId string,
+	emailEnabled string,
+	emailInboundFlowId string,
+	messageEnabled string,
+	messageInboundFlowId string,
+	extraArgs ...string) string {
+	return fmt.Sprintf(` direct_routing {
+		agent_wait_seconds = %s
+		wait_for_agent = %s
+		call_enabled = %s
+		call_inbound_flow_id = "%s"
+		voicemail_flow_id = "%s"
+		email_enabled = %s
+		email_inbound_flow_id = "%s"
+		message_enabled = %s
+		message_inbound_flow_id = "%s"
+		%s
+	}
+	`,
+	agentWaitSeconds,
+	waitForAgent,
+	callEnabled,
+	callInboundFlowId,
+	voicemailFlowId,
+	emailEnabled,
+	emailInboundFlowId,
+	messageEnabled,
+	messageInboundFlowId,
+	strings.Join(extraArgs, "\n"))
+}
+
 func validateRoutingRules(resourceName string, ringNum int, operator string, threshold string, waitSec string) resource.TestCheckFunc {
 	ringNumStr := strconv.Itoa(ringNum)
 	return resource.ComposeAggregateTestCheckFunc(
@@ -780,6 +899,29 @@ func validateQueueWrapupCode(queueResourceName string, codeResourceName string) 
 		}
 		return fmt.Errorf("Wrapup code %s not found for queue %s in state", codeID, queueID)
 	}
+}
+
+func validateDirectRouting(resourceName string, 
+	agentWaitSeconds string, 
+	waitForAgent string, 
+	callEnabled string,
+	callInboundFlowId string,
+	voicemailFlowId string,
+	emailEnabled string,
+	emailInboundFlowId string,
+	messageEnabled string,
+	messageInboundFlowId string,) resource.TestCheckFunc {
+	return resource.ComposeAggregateTestCheckFunc(
+		resource.TestCheckResourceAttr("genesyscloud_routing_queue."+resourceName, "direct_routing.0.agent_wait_seconds",		 agentWaitSeconds),
+		resource.TestCheckResourceAttr("genesyscloud_routing_queue."+resourceName, "direct_routing.0.wait_for_agent",		 	 waitForAgent),
+		resource.TestCheckResourceAttr("genesyscloud_routing_queue."+resourceName, "direct_routing.0.call_enabled", 			 callEnabled),
+		resource.TestCheckResourceAttr("genesyscloud_routing_queue."+resourceName, "direct_routing.0.call_inbound_flow_id", 	 callInboundFlowId),
+		resource.TestCheckResourceAttr("genesyscloud_routing_queue."+resourceName, "direct_routing.0.voicemail_flow_id", 		 voicemailFlowId),
+		resource.TestCheckResourceAttr("genesyscloud_routing_queue."+resourceName, "direct_routing.0.email_enabled", 			 emailEnabled),
+		resource.TestCheckResourceAttr("genesyscloud_routing_queue."+resourceName, "direct_routing.0.email_inbound_flow_id", 	 emailInboundFlowId),
+		resource.TestCheckResourceAttr("genesyscloud_routing_queue."+resourceName, "direct_routing.0.message_enabled", 		 messageEnabled),
+		resource.TestCheckResourceAttr("genesyscloud_routing_queue."+resourceName, "direct_routing.0.message_inbound_flow_id", messageInboundFlowId),
+	)
 }
 
 func TestAccResourceRoutingQueueSkillGroups(t *testing.T) {
