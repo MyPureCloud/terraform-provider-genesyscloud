93e40d72bbd2f20a884d5e86af42c1612cb0c6ad
John Carnell <john.carnell@genesys.com>
2023-02-10 13:42:30 -0500

    DEVENGAGE-1557: Latest changes

--- genesyscloud/resource_genesyscloud_integration_action.go
+++ genesyscloud/resource_genesyscloud_integration_action.go
@@ -16,7 +16,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/mypurecloud/platform-client-sdk-go/v91/platformclientv2"
+	"github.com/mypurecloud/platform-client-sdk-go/v92/platformclientv2"
 )
 
 var (
