a14c072559bed778374a016366b22e26b3bb9890
Jayashankar Chaluvaraj <jshankarc1@gmail.com>
2023-03-03 11:20:59 +0000

    GPE-12605: Refactored & Relocated ActionMap test cases

--- test/data/resource/genesyscloud_journey_action_map/action_media_types/01_create_web_messaging_offer_without_config.tf
+++ test/data/resource/genesyscloud_journey_action_map/action_media_types/01_create_web_messaging_offer_without_config.tf
@@ -23,29 +23,3 @@ resource "genesyscloud_journey_segment" "terraform_test_-TEST-CASE-_action_map_d
     source = "AdobeExperiencePlatform"
   }
 }
-
-resource "genesyscloud_journey_action_map" "terraform_test_-TEST-CASE-2" {
-  display_name          = "terraform_test_-TEST-CASE-2"
-  trigger_with_event_conditions {
-  key = "page.title"
-  values = ["mytitle"]
-  operator = "equal"
-  event_name = "page_viewed"
-  session_type = "web"
-  stream_type = "Web"
-  }
-  action {
-    media_type = "webMessagingOffer"
-    is_pacing_enabled = false
-    web_messaging_offer_fields {
-      offer_text = "Hey how're you?"
-    }
-  }
-  activation {
-    type = "immediate"
-  }
-  start_date = "2023-01-02T15:04:05.000000"
-  weight = 2
-  is_active = false
-  ignore_frequency_cap = false
-}
