987ca9fae567b04d8ebdac6dc1484ddb44936c6f
John Carnell <john.carnell@genesys.com>
2023-03-07 13:01:01 -0500

    DEVENGAGE-1672: Added some error checks to code

--- genesyscloud/data_source_genesyscloud_webdeployments_deployment.go
+++ genesyscloud/data_source_genesyscloud_webdeployments_deployment.go
@@ -3,6 +3,7 @@ package genesyscloud
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -32,10 +33,14 @@ func dataSourceDeploymentRead(ctx context.Context, d *schema.ResourceData, m int
 	name := d.Get("name").(string)
 
 	return withRetries(ctx, 15*time.Second, func() *resource.RetryError {
-		deployments, _, err := api.GetWebdeploymentsDeployments([]string{})
+		deployments, resp, err := api.GetWebdeploymentsDeployments([]string{})
 
-		if err != nil {
-			return resource.NonRetryableError(fmt.Errorf("Error retrieving web deployment %s: %s", name, err))
+		if err != nil && resp.StatusCode == http.StatusNotFound {
+			return resource.RetryableError(fmt.Errorf("No web deployment record found %s: %s. Correlation id: %s", name, err, resp.CorrelationID))
+		}
+
+		if err != nil && resp.StatusCode != http.StatusNotFound {
+			return resource.NonRetryableError(fmt.Errorf("Error retrieving web deployment %s: %s. Correlation id: %s", name, err, resp.CorrelationID))
 		}
 
 		for _, deployment := range *deployments.Entities {
