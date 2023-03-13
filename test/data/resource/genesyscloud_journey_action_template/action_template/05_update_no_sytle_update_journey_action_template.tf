634df5b2a28e165caa0e0ad132c84106f710474f
Jayashankar Chaluvaraj <jshankarc1@gmail.com>
2023-03-13 13:01:59 +0000

    GPE-12612: Added Action Template Resource, Datasource and Exports (#469)
    * GPE-12612: Added Action Template New Resource and test
    
    * GPE-12612: Refactored some code and Added journey_action_template md file
    
    * GPE-12612: Fixed Create with background_color
    
    * GPE-12612: Added export and data source functionality
    
    * Squashed commit of the following:
    
    commit 00b1ee41c3a60429a6ac8d9ec9af936b8302f33c
    Author: Charlie Conneely <87772562+charliecon@users.noreply.github.com>
    Date:   Wed Mar 8 14:55:40 2023 +0000
    
        Fixing org auth pairing tests (#463)
    
    commit 87cccf70d7b35eafaf1d764282dd8c9f28e9e159
    Author: Charlie Conneely <87772562+charliecon@users.noreply.github.com>
    Date:   Wed Mar 8 14:46:50 2023 +0000
    
        Fixing auth division tests (#464)
    
    commit 434b428f48e667986535253a471788e7b629da6f
    Author: Charlie Conneely <87772562+charliecon@users.noreply.github.com>
    Date:   Wed Mar 8 14:41:52 2023 +0000
    
        [no-jira] action journey map test fix and removed log statement (#462)
    
        * Fixed journey action maps test
    
        * Removed register log statement in provider.go
    
    commit c7adcb12dfd9fb0ff35c2eaf853a4cd43d885c2c
    Author: Jayashankar Chaluvaraj <jshankarc1@gmail.com>
    Date:   Tue Mar 7 11:16:37 2023 +0000
    
        GPE-12605: Added is_pacing_enabled & trigger_with_segments is an optional parameter (#457)
    
        * GPE-12605: changes trigger_with_segments as optional parameter & added a new attribute is_pacing_enabled feature
    
        * GPE-12605: improved code, actionmap patch method updated to handle IsPacingEnabled, updated md file with latest schema changes & added test cases
    
        * GPE-12605: Refactored & Relocated ActionMap test cases
    
        ---------
    
        Co-authored-by: Jayashankar Chaluvaraj <jayashankar.chaluvaraj@genesys.com>
    
    commit 015e5d5d74fc7c328cfb5f0ff0bbcdaa3da9dbc1
    Author: dginty4 <108797778+dginty4@users.noreply.github.com>
    Date:   Mon Mar 6 11:42:46 2023 +0000
    
        Added message about JsonNode object (#458)
    
    * GPE-12612: Added examples, api md updated, documents update and some code fixes
    
    * GPE-12612: Added journey_action_template.md file
    
    * GPE-12612: Added some tests
    
    * Revert "Merge branch 'main' into GPE-12612-Action-Template"
    
    This reverts commit 8e447ec09322029a39c4964c824b382ecb812065, reversing
    changes made to 691e09d19153b64ae75b5d9de08806ea10ab8dd9.
    
    * GPE-12612: Removed unnecessary clean up steps

---
+++ test/data/resource/genesyscloud_journey_action_template/action_template/05_update_no_sytle_update_journey_action_template.tf
@@ -0,0 +1,19 @@
+resource "genesyscloud_journey_action_template" "terraform_test_-TEST-CASE-" {
+  name        = "terraform_test_-TEST-CASE-"
+  description = "Text and image content offer"
+  media_type  = "contentOffer"
+  state       = "Active"
+  content_offer {
+    image_url    = "https://api-cdn.inindca.com/uploads/v1/publicassets/images/d460a77c-9870-404f-9711-4be1cc247b66/d7c29719-095b-45d3-9ceb-f1368bcfcf3f.dragon.png"
+    display_mode = "Modal"
+    layout_mode  = "RightText"
+    title        = "Dragon!"
+    headline     = "Save 100%"
+    body         = "Book now and add discount code 123456 at checkout to save 100%"
+    call_to_action {
+      text   = "Dragon!"
+      url    = "https://www.genesys.com"
+      target = "Self"
+    }
+  }
+}
