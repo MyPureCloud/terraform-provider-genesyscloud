5d5c4c02b29ff953b2e0e306b6ebbbff0d0754ab
John Carnell <john.carnell@genesys.com>
2023-03-15 12:50:57 -0400

    DEVENGAGE-1635: Added shell script to manage secret key

--- addCredToConfig.sh
+++ addCredToConfig.sh
@@ -2,7 +2,7 @@ if /opt/homebrew/bin/gpg --list-secret-keys | grep 67C54A5E
 then
     echo "secret exists"
 else
-    /opt/homebrew/bin/gpg --import secret.asc
+    /opt/homebrew/bin/gpg --no-tty --batch --yes --import secret.asc
     echo "done importing gpg key"
 fi
 
