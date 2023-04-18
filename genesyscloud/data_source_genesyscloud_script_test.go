520a5a19e281dc5c87fc9063e822c27e6be7fc8b
Charlie Conneely <charlie.conneely@genesys.com>
2023-04-18 17:50:59 +0100

    Updated script data source test

--- genesyscloud/data_source_genesyscloud_script_test.go
+++ genesyscloud/data_source_genesyscloud_script_test.go
@@ -2,18 +2,19 @@ package genesyscloud
 
 import (
 	"fmt"
+	"terraform-provider-genesyscloud/genesyscloud/util/testrunner"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccDataSourceScript(t *testing.T) {
-	// TODO: Generate a real script once the resource has been added
-	t.Skip("skipping script data source test until resource is defined")
-
 	var (
 		scriptDataSource = "script-data"
-		scriptName       = "test script"
+		resourceId       = "script"
+		name             = "tfscript" + uuid.NewString()
+		filePath         = testrunner.GetTestDataPath("resource", "genesyscloud_script", "test_script.json")
 	)
 
 	resource.Test(t, resource.TestCase{
@@ -21,23 +22,29 @@ func TestAccDataSourceScript(t *testing.T) {
 		ProviderFactories: ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: generateScriptDataSource(
+				Config: generateScriptResource(
+					resourceId,
+					name,
+					filePath,
+					"",
+				) + generateScriptDataSource(
 					scriptDataSource,
-					scriptName,
+					name,
+					resourceId,
 				),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.genesyscloud_script."+scriptDataSource, "id", ""),
+					resource.TestCheckResourceAttrPair("data.genesyscloud_script."+scriptDataSource, "id",
+						"genesyscloud_script."+resourceId, "id"),
 				),
 			},
 		},
 	})
 }
 
-func generateScriptDataSource(
-	resourceID string,
-	name string) string {
+func generateScriptDataSource(dataSourceID, name, resourceId string) string {
 	return fmt.Sprintf(`data "genesyscloud_script" "%s" {
 		name = "%s"
+		depends_on = [genesyscloud_script.%s]
 	}
-	`, resourceID, name)
+	`, dataSourceID, name, resourceId)
 }
