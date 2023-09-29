1f50d461ac915f98fc49c6d2af3669e3d240cd63
Charlie Conneely <87772562+charliecon@users.noreply.github.com>
2023-09-29 18:30:40 +0100

    go generate (#645)
    * go generate
    
    * Fixing tests afer changes made in PR #643
    
    * Removing unused variables

--- genesyscloud/resource_genesyscloud_routing_queue.go
+++ genesyscloud/resource_genesyscloud_routing_queue.go
@@ -8,11 +8,10 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"terraform-provider-genesyscloud/genesyscloud/consistency_checker"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
-
-	"terraform-provider-genesyscloud/genesyscloud/consistency_checker"
 
 	resourceExporter "terraform-provider-genesyscloud/genesyscloud/resource_exporter"
 	lists "terraform-provider-genesyscloud/genesyscloud/util/lists"
@@ -25,12 +24,6 @@ import (
 )
 
 var (
-	mediaSettingsKeyCall     = "call"
-	mediaSettingsKeyCallback = "callback"
-	mediaSettingsKeyChat     = "chat"
-	mediaSettingsKeyEmail    = "email"
-	mediaSettingsKeyMessage  = "message"
-
 	bullseyeExpansionTypeTimeout = "TIMEOUT_SECONDS"
 
 	memberGroupResource = &schema.Resource{
