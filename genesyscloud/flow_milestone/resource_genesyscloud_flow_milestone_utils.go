16b2fcb7ea0011e641420f5e816d12757e9e9de2
dginty4 <declan.ginty@genesys.com>
2023-11-07 12:37:49 +0000

    Added comment to utils file

--- genesyscloud/flow_milestone/resource_genesyscloud_flow_milestone_utils.go
+++ genesyscloud/flow_milestone/resource_genesyscloud_flow_milestone_utils.go
@@ -5,6 +5,11 @@ import (
 	"github.com/mypurecloud/platform-client-sdk-go/v115/platformclientv2"
 )
 
+/*
+The resource_genesyscloud_flow_outcome_utils.go file contains various helper methods to marshal
+and unmarshal data into formats consumable by Terraform and/or Genesys Cloud.
+*/
+
 // getFlowMilestoneFromResourceData maps data from schema ResourceData object to a platformclientv2.Flowmilestone
 func getFlowMilestoneFromResourceData(d *schema.ResourceData) platformclientv2.Flowmilestone {
 	divisionId := d.Get("division_id").(string)
