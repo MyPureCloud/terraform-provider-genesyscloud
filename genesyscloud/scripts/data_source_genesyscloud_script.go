3d47e349fe03fcb2a25ba256d4b97bcead807693
Charlie Conneely <charlie.conneely@genesys.com>
2023-11-21 15:48:22 +0000

    Adding update context to scripts resource + refactoring

--- genesyscloud/scripts/data_source_genesyscloud_script.go
+++ genesyscloud/scripts/data_source_genesyscloud_script.go
@@ -2,7 +2,6 @@ package scripts
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
@@ -25,24 +24,15 @@ func dataSourceScriptRead(ctx context.Context, d *schema.ResourceData, m interfa
 	name := d.Get("name").(string)
 
 	// Query for scripts by name. Retry in case new script is not yet indexed by search.
-	// As script names are non-unique, fail in case of multiple results.
 	return gcloud.WithRetries(ctx, 15*time.Second, func() *retry.RetryError {
-		scripts, err := scriptsProxy.getPublishedScriptsByName(ctx, name)
-
+		scriptId, retryable, err := scriptsProxy.getScriptIdByName(ctx, name)
 		if err != nil {
-			return retry.NonRetryableError(fmt.Errorf("Error requesting script %s: %s", name, err))
+			if retryable {
+				return retry.RetryableError(err)
+			}
+			return retry.NonRetryableError(err)
 		}
-
-		if len(*scripts) == 0 {
-			return retry.RetryableError(fmt.Errorf("No scripts found with name %s", name))
-		}
-
-		if len(*scripts) > 1 {
-			return retry.NonRetryableError(fmt.Errorf("Ambiguous script name: %s", name))
-		}
-
-		script := (*scripts)[0]
-		d.SetId(*script.Id)
+		d.SetId(scriptId)
 		return nil
 	})
 }
---
+++ genesyscloud/scripts/resource_genesyscloud_script_utils.go
@@ -0,0 +1,38 @@
+package scripts
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+	gcloud "terraform-provider-genesyscloud/genesyscloud"
+	"terraform-provider-genesyscloud/genesyscloud/util/files"
+)
+
+// ScriptResolver is used to download all Genesys Cloud scripts from Genesys Cloud
+func ScriptResolver(scriptId, exportDirectory, subDirectory string, configMap map[string]interface{}, meta interface{}) error {
+	sdkConfig := meta.(*gcloud.ProviderMeta).ClientConfig
+	scriptsProxy := getScriptsProxy(sdkConfig)
+
+	exportFileName := fmt.Sprintf("script-%s.json", scriptId)
+
+	fullPath := path.Join(exportDirectory, subDirectory)
+	if err := os.MkdirAll(fullPath, os.ModePerm); err != nil {
+		return err
+	}
+	ctx := context.Background()
+	url, err := scriptsProxy.getScriptExportUrl(ctx, scriptId)
+	if err != nil {
+		return err
+	}
+
+	if err := files.DownloadExportFile(fullPath, exportFileName, url); err != nil {
+		return err
+	}
+
+	// Update filepath field in configMap to point to exported script file
+	configMap["filepath"] = path.Join(subDirectory, exportFileName)
+	configMap["file_content_hash"] = fmt.Sprintf(`${filesha256("%s")}`, path.Join(subDirectory, exportFileName))
+
+	return err
+}
