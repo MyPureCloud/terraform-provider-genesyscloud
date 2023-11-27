56b7b9223aa80839310c335b4ae08d838549825e
HemanthDogiparthi12 <127493303+HemanthDogiparthi12@users.noreply.github.com>
2023-11-27 13:52:29 +0000

    Add Auth Product DataSource (#723)
    * Add Auth Product DataSource

---
+++ genesyscloud/authorization_product/data_source_genesyscloud_authorization_product.go
@@ -0,0 +1,46 @@
+package authorization_product
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+
+	gcloud "terraform-provider-genesyscloud/genesyscloud"
+)
+
+func dataSourceAuthorizationProductRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	sdkConfig := meta.(*gcloud.ProviderMeta).ClientConfig
+	proxy := getauthProductProxy(sdkConfig)
+	name := d.Get("name").(string)
+
+	return gcloud.WithRetries(ctx, 15*time.Second, func() *retry.RetryError {
+		// Get the list of enabled products
+		authProductId, retryable, err := proxy.getAuthorizationProduct(ctx, name)
+
+		if err != nil {
+			if retryable {
+				return retry.RetryableError(err)
+			}
+			return retry.NonRetryableError(err)
+		}
+
+		d.SetId(authProductId)
+		return nil
+	})
+}
+
+func GenerateAuthorizationProductDataSource(id, productName, dependsOn string) string {
+	return fmt.Sprintf(`
+data "genesyscloud_authorization_product" "%s" {
+	name = "%s"
+	depends_on=[%s]
+}
+`, id, productName, dependsOn)
+}
---
+++ genesyscloud/authorization_product/genesyscloud_authorization_product_schema.go
@@ -0,0 +1,40 @@
+package authorization_product
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	gcloud "terraform-provider-genesyscloud/genesyscloud"
+	registrar "terraform-provider-genesyscloud/genesyscloud/resource_register"
+)
+
+/*
+genesyscloud_authorization_product_schema holds four functions within it:
+
+1.  The registration code that registers the Datasource for the package.
+2.  The datasource schema definitions for the authorization_product datasource.
+*/
+const resourceName = "genesyscloud_authorization_product"
+
+// SetRegistrar registers all of the resources, datasources and exporters in the package
+func SetRegistrar(regInstance registrar.Registrar) {
+	regInstance.RegisterDataSource(resourceName, DataSourceAuthorizationProduct())
+}
+
+// DataSourceAuthorizationProduct registers the authorization_product data source
+func DataSourceAuthorizationProduct() *schema.Resource {
+	return &schema.Resource{
+		Description: `Data source for Genesys Cloud Authorisation Products.`,
+
+		ReadContext: gcloud.ReadWithPooledClient(dataSourceAuthorizationProductRead),
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		SchemaVersion: 1,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Description: `Authorization Product name.`,
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+		},
+	}
+}
