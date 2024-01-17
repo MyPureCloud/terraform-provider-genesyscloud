bab35c02b84166e8a9fca72a668101493342ec47
kavinbalagen <150852268+kavinbalagen@users.noreply.github.com>
2024-01-17 08:04:23 -0500

    fresh branch to avoid merge conflicts (#795)

--- examples/resources/genesyscloud_team/resource.tf
+++ examples/resources/genesyscloud_team/resource.tf
@@ -2,4 +2,5 @@ resource "genesyscloud_team" "example_team" {
   name        = "My Team"
   description = "Example Team"
   division_id = genesyscloud_auth_division.example_division.id
+  member_ids  = []
 }
