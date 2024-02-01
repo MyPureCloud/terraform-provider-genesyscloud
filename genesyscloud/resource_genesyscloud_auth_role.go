7308e9c0a78688c9ff161e5bf7cef65f1cbdd2bd
dginty4 <declan.ginty@genesys.com>
2024-02-01 12:36:51 +0000

    Added validation for permission policies

--- genesyscloud/resource_genesyscloud_auth_role.go
+++ genesyscloud/resource_genesyscloud_auth_role.go
@@ -216,12 +216,26 @@ func createAuthRole(ctx context.Context, d *schema.ResourceData, meta interface{
 		return updateAuthRole(ctx, d, meta)
 	}
 
-	role, _, err := authAPI.PostAuthorizationRoles(platformclientv2.Domainorganizationrolecreate{
+	roleObj := platformclientv2.Domainorganizationrolecreate{
 		Name:               &name,
 		Description:        &description,
 		Permissions:        buildSdkRolePermissions(d),
 		PermissionPolicies: buildSdkRolePermPolicies(d),
-	})
+	}
+
+	// Validate each permission policy exists before continuing
+	// This is a workaround for a bug in the auth roles APIs
+	// Bug reported to auth team in ticket AUTHZ-315
+	if roleObj.PermissionPolicies != nil {
+		for _, policy := range *roleObj.PermissionPolicies {
+			err := validatePermissionPolicy(authAPI, &policy)
+			if err != nil {
+				return diag.Errorf("Permission policy not found: %s", err)
+			}
+		}
+	}
+
+	role, _, err := authAPI.PostAuthorizationRoles(roleObj)
 	if err != nil {
 		return diag.Errorf("Failed to create role %s: %s", name, err)
 	}
@@ -287,13 +301,28 @@ func updateAuthRole(ctx context.Context, d *schema.ResourceData, meta interface{
 	authAPI := platformclientv2.NewAuthorizationApiWithConfig(sdkConfig)
 
 	log.Printf("Updating role %s", name)
-	_, _, err := authAPI.PutAuthorizationRole(d.Id(), platformclientv2.Domainorganizationroleupdate{
+
+	updateObj := platformclientv2.Domainorganizationroleupdate{
 		Name:               &name,
 		Description:        &description,
 		Permissions:        buildSdkRolePermissions(d),
 		PermissionPolicies: buildSdkRolePermPolicies(d),
 		DefaultRoleId:      &defaultRoleID,
-	})
+	}
+
+	// Validate each permission policy exists before continuing
+	// This is a workaround for a bug in the auth roles APIs
+	// Bug reported to auth team in ticket AUTHZ-315
+	if updateObj.PermissionPolicies != nil {
+		for _, policy := range *updateObj.PermissionPolicies {
+			err := validatePermissionPolicy(authAPI, &policy)
+			if err != nil {
+				return diag.Errorf("Permission policy not found: %s", err)
+			}
+		}
+	}
+
+	_, _, err := authAPI.PutAuthorizationRole(d.Id(), updateObj)
 	if err != nil {
 		return diag.Errorf("Failed to update role %s: %s", name, err)
 	}
@@ -528,6 +557,81 @@ func getRoleID(defaultRoleID string, authAPI *platformclientv2.AuthorizationApi)
 	}
 
 	return *(*roles.Entities)[0].Id, nil
+}
+
+func validatePermissionPolicy(authApi *platformclientv2.AuthorizationApi, policy *platformclientv2.Domainpermissionpolicy) error {
+	allowedPermissions, err := getAllowedPermissions(authApi, *policy.Domain)
+	if err != nil {
+		return fmt.Errorf("error requesting org permissions: %s", err)
+	}
+	if allowedPermissions == nil {
+		return fmt.Errorf("domain %s not found", *policy.Domain)
+	}
+
+	// Check entity type (e.g. callableTimeSet) exists in the map of allowed permissions
+	if entityPermissions, ok := (*allowedPermissions)[*policy.EntityName]; ok {
+		// Check if the policy actions exist for the given domain permission e.g. callableTimeSet: add
+		for _, action := range *policy.ActionSet {
+			if action == "*" && len(entityPermissions) >= 1 {
+				break
+			}
+
+			var found bool
+			for _, entityPermission := range entityPermissions {
+				if action == *entityPermission.Action {
+					// action found, move to next action
+					found = true
+					break
+				}
+			}
+			if !found {
+				return fmt.Errorf("action %s not found for domain %s, entity name %s", action, *policy.Domain, *policy.EntityName)
+			}
+		}
+		// All actions have been found, permission exists
+		return nil
+	}
+
+	return fmt.Errorf("entity_name %s not found for domain %s", *policy.EntityName, *policy.Domain)
+}
+
+// getAllowedPermissions will get all allowed permissions for a domain
+func getAllowedPermissions(authApi *platformclientv2.AuthorizationApi, domain string) (*map[string][]platformclientv2.Domainpermission, error) {
+	const pageSize = 100
+	allowedPermissions := make(map[string][]platformclientv2.Domainpermission)
+
+	permissions, _, err := authApi.GetAuthorizationPermissions(pageSize, 1, "domain", domain)
+	if err != nil {
+		return nil, err
+	}
+
+	if permissions.Entities == nil || len(*permissions.Entities) == 0 {
+		return &allowedPermissions, nil
+	}
+
+	for _, permission := range *permissions.Entities {
+		for entityType, entityPermissions := range *permission.PermissionMap {
+			allowedPermissions[entityType] = entityPermissions
+		}
+	}
+
+	for pageNum := 2; pageNum <= *permissions.PageCount; pageNum++ {
+		permissions, _, err := authApi.GetAuthorizationPermissions(pageSize, pageNum, "domain", domain)
+		if err != nil {
+			return nil, err
+		}
+		if permissions.Entities == nil || len(*permissions.Entities) == 0 {
+			break
+		}
+
+		for _, permission := range *permissions.Entities {
+			for entityType, entityPermissions := range *permission.PermissionMap {
+				allowedPermissions[entityType] = entityPermissions
+			}
+		}
+	}
+
+	return &allowedPermissions, nil
 }
 
 func GenerateAuthRoleResource(
