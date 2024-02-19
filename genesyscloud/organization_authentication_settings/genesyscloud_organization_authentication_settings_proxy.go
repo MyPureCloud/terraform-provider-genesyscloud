c6c2d4f0f0cc3fe1f9a167db46bc8c034152ac89
Brian Moyles <brian.moyles@genesys.com>
2024-02-19 14:12:46 +0000

    Created test for Organization Authentication Settings

---
+++ genesyscloud/organization_authentication_settings/genesyscloud_organization_authentication_settings_proxy.go
@@ -0,0 +1,78 @@
+package organization_authentication_settings
+
+import (
+	"context"
+	"fmt"
+	"github.com/mypurecloud/platform-client-sdk-go/v121/platformclientv2"
+)
+
+/*
+The genesyscloud_organization_authentication_settings_proxy.go file contains the proxy structures and methods that interact
+with the Genesys Cloud SDK. We use composition here for each function on the proxy so individual functions can be stubbed
+out during testing.
+*/
+
+// internalProxy holds a proxy instance that can be used throughout the package
+var internalProxy *orgAuthSettingsProxy
+
+// Type definitions for each func on our proxy so we can easily mock them out later
+type getOrgAuthSettingsByIdFunc func(ctx context.Context, p *orgAuthSettingsProxy, id string) (orgAuthSettings *platformclientv2.Orgauthsettings, responseCode int, err error)
+type updateOrgAuthSettingsFunc func(ctx context.Context, p *orgAuthSettingsProxy, orgAuthSettings *platformclientv2.Orgauthsettings) (*platformclientv2.Orgauthsettings, int, error)
+
+// orgAuthSettingsProxy contains all of the methods that call genesys cloud APIs.
+type orgAuthSettingsProxy struct {
+	clientConfig               *platformclientv2.Configuration
+	organizationApi            *platformclientv2.OrganizationApi
+	getOrgAuthSettingsByIdAttr getOrgAuthSettingsByIdFunc
+	updateOrgAuthSettingsAttr  updateOrgAuthSettingsFunc
+}
+
+// newOrgAuthSettingsProxy initializes the organization authentication settings proxy with all of the data needed to communicate with Genesys Cloud
+func newOrgAuthSettingsProxy(clientConfig *platformclientv2.Configuration) *orgAuthSettingsProxy {
+	api := platformclientv2.NewOrganizationApiWithConfig(clientConfig)
+	return &orgAuthSettingsProxy{
+		clientConfig:               clientConfig,
+		organizationApi:            api,
+		getOrgAuthSettingsByIdAttr: getOrgAuthSettingsByIdFn,
+		updateOrgAuthSettingsAttr:  updateOrgAuthSettingsFn,
+	}
+}
+
+// getOrgAuthSettingsProxy acts as a singleton to for the internalProxy.  It also ensures
+// that we can still proxy our tests by directly setting internalProxy package variable
+func getOrgAuthSettingsProxy(clientConfig *platformclientv2.Configuration) *orgAuthSettingsProxy {
+	if internalProxy == nil {
+		internalProxy = newOrgAuthSettingsProxy(clientConfig)
+	}
+
+	return internalProxy
+}
+
+// getOrgAuthSettingsById returns a single Genesys Cloud organization authentication settings by Id
+func (p *orgAuthSettingsProxy) getOrgAuthSettingsById(ctx context.Context, id string) (orgAuthSettings *platformclientv2.Orgauthsettings, statusCode int, err error) {
+	return p.getOrgAuthSettingsByIdAttr(ctx, p, id)
+}
+
+// updateOrgAuthSettings updates a Genesys Cloud organization authentication settings
+func (p *orgAuthSettingsProxy) updateOrgAuthSettings(ctx context.Context, orgAuthSettings *platformclientv2.Orgauthsettings) (*platformclientv2.Orgauthsettings, int, error) {
+	return p.updateOrgAuthSettingsAttr(ctx, p, orgAuthSettings)
+}
+
+// getOrgAuthSettingsByIdFn is an implementation of the function to get a Genesys Cloud organization authentication settings by Id
+func getOrgAuthSettingsByIdFn(ctx context.Context, p *orgAuthSettingsProxy, id string) (orgAuthSettings *platformclientv2.Orgauthsettings, statusCode int, err error) {
+	orgAuthSettings, resp, err := p.organizationApi.GetOrganizationsAuthenticationSettings()
+	if err != nil {
+		return nil, resp.StatusCode, fmt.Errorf("Failed to retrieve organization authentication settings by id %s: %s", id, err)
+	}
+
+	return orgAuthSettings, resp.StatusCode, nil
+}
+
+// updateOrgAuthSettingsFn is an implementation of the function to update a Genesys Cloud organization authentication settings
+func updateOrgAuthSettingsFn(ctx context.Context, p *orgAuthSettingsProxy, orgAuthSettings *platformclientv2.Orgauthsettings) (*platformclientv2.Orgauthsettings, int, error) {
+	orgAuthSettings, resp, err := p.organizationApi.PatchOrganizationsAuthenticationSettings(*orgAuthSettings)
+	if err != nil {
+		return nil, 0, fmt.Errorf("Failed to update organization authentication settings: %s", err)
+	}
+	return orgAuthSettings, resp.StatusCode, nil
+}
---
+++ genesyscloud/organization_authentication_settings/resource_genesyscloud_organization_authentication_settings.go
@@ -0,0 +1,86 @@
+package organization_authentication_settings
+
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/mypurecloud/platform-client-sdk-go/v121/platformclientv2"
+	"log"
+	"terraform-provider-genesyscloud/genesyscloud/consistency_checker"
+	resourceExporter "terraform-provider-genesyscloud/genesyscloud/resource_exporter"
+
+	gcloud "terraform-provider-genesyscloud/genesyscloud"
+
+	"terraform-provider-genesyscloud/genesyscloud/util/resourcedata"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+)
+
+/*
+The resource_genesyscloud_organization_authentication_settings.go contains all of the methods that perform the core logic for a resource.
+*/
+
+func getAllOrganizationAuthenticationSettings(_ context.Context, clientConfig *platformclientv2.Configuration) (resourceExporter.ResourceIDMetaMap, diag.Diagnostics) {
+	resources := make(resourceExporter.ResourceIDMetaMap)
+	resources["0"] = &resourceExporter.ResourceMeta{Name: "organization_authentication_settings"}
+	return resources, nil
+}
+
+// createOrganizationAuthenticationSettings is used by the organization_authentication_settings resource to create Genesys cloud organization authentication settings
+func createOrganizationAuthenticationSettings(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	log.Printf("Creating Organization Authentication Settings")
+	d.SetId("settings")
+	return updateOrganizationAuthenticationSettings(ctx, d, meta)
+}
+
+// readOrganizationAuthenticationSettings is used by the organization_authentication_settings resource to read an organization authentication settings from genesys cloud
+func readOrganizationAuthenticationSettings(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	sdkConfig := meta.(*gcloud.ProviderMeta).ClientConfig
+	proxy := getOrgAuthSettingsProxy(sdkConfig)
+
+	log.Printf("Reading organization authentication settings %s", d.Id())
+
+	return gcloud.WithRetriesForRead(ctx, d, func() *retry.RetryError {
+		orgAuthSettings, respCode, getErr := proxy.getOrgAuthSettingsById(ctx, d.Id())
+		if getErr != nil {
+			if gcloud.IsStatus404ByInt(respCode) {
+				return retry.RetryableError(fmt.Errorf("Failed to read organization authentication settings %s: %s", d.Id(), getErr))
+			}
+			return retry.NonRetryableError(fmt.Errorf("Failed to read organization authentication settings %s: %s", d.Id(), getErr))
+		}
+
+		cc := consistency_checker.NewConsistencyCheck(ctx, d, meta, ResourceOrganizationAuthenticationSettings())
+
+		resourcedata.SetNillableValue(d, "multifactor_authentication_required", orgAuthSettings.MultifactorAuthenticationRequired)
+		resourcedata.SetNillableValue(d, "domain_allowlist_enabled", orgAuthSettings.DomainAllowlistEnabled)
+		resourcedata.SetNillableValue(d, "domain_allowlist", orgAuthSettings.DomainAllowlist)
+		resourcedata.SetNillableValue(d, "ip_address_allowlist", orgAuthSettings.IpAddressAllowlist)
+		resourcedata.SetNillableValueWithInterfaceArrayWithFunc(d, "password_requirements", orgAuthSettings.PasswordRequirements, flattenPasswordRequirements)
+
+		log.Printf("Read organization authentication settings %s", d.Id())
+		return cc.CheckState()
+	})
+}
+
+// updateOrganizationAuthenticationSettings is used by the organization_authentication_settings resource to update an organization authentication settings in Genesys Cloud
+func updateOrganizationAuthenticationSettings(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	sdkConfig := meta.(*gcloud.ProviderMeta).ClientConfig
+	proxy := getOrgAuthSettingsProxy(sdkConfig)
+	AuthSettings := getOrganizationAuthenticationSettingsFromResourceData(d)
+
+	log.Printf("Updating organization authentication settings %s", d.Id())
+
+	orgAuthSettings, _, err := proxy.updateOrgAuthSettings(ctx, &AuthSettings)
+	if err != nil {
+		return diag.Errorf("Failed to update organization authentication settings: %s", err)
+	}
+
+	log.Printf("Updated organization authentication settings %s %s", d.Id, orgAuthSettings)
+	return readOrganizationAuthenticationSettings(ctx, d, meta)
+}
+
+// deleteOrganizationAuthenticationSettings is used by the organization_authentication_settings resource to delete an organization authentication settings from Genesys cloud
+func deleteOrganizationAuthenticationSettings(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return nil
+}
