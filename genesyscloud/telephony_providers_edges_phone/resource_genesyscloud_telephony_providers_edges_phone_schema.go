0e074914fdfdfce5931765195b5c21db079e095d
kavinbalagen <150852268+kavinbalagen@users.noreply.github.com>
2024-02-29 22:06:21 +0530

    Revert "Devtooling 287 (#874)" (#886)
    This reverts commit 929572f50d50d7d26e692228e44cb081abad8e63.

--- genesyscloud/telephony_providers_edges_phone/resource_genesyscloud_telephony_providers_edges_phone_schema.go
+++ genesyscloud/telephony_providers_edges_phone/resource_genesyscloud_telephony_providers_edges_phone_schema.go
@@ -140,13 +140,6 @@ func ResourcePhone() *schema.Resource {
 				Computed:    true,
 				Elem:        &schema.Schema{Type: schema.TypeString, ValidateDiagFunc: gcloud.ValidatePhoneNumber},
 			},
-			"properties": {
-				Description:      "phone properties",
-				Type:             schema.TypeString,
-				Optional:         true,
-				Computed:         true,
-				DiffSuppressFunc: gcloud.SuppressEquivalentJsonDiffs,
-			},
 			"capabilities": {
 				Description: "Phone Capabilities.",
 				Type:        schema.TypeList,
@@ -156,7 +149,6 @@ func ResourcePhone() *schema.Resource {
 				Elem:        phoneCapabilities,
 			},
 		},
-		CustomizeDiff: gcloud.CustomizePhonePropertiesDiff,
 	}
 }
 
