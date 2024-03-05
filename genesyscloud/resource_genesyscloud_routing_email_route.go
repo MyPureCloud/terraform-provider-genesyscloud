16a708889453d27d95b1b475dfc2eabf07a5e5b8
Brian Moyles <brian.moyles@genesys.com>
2024-03-05 16:17:40 +0000

    Fixed GetALl API call that prevented exporting resources

--- genesyscloud/resource_genesyscloud_routing_email_route.go
+++ genesyscloud/resource_genesyscloud_routing_email_route.go
@@ -43,7 +43,7 @@ func getAllRoutingEmailRoutes(_ context.Context, clientConfig *platformclientv2.
 
 	for pageNum := 1; ; pageNum++ {
 		const pageSize = 100
-		domains, _, getErr := routingAPI.GetRoutingEmailDomains(pageNum, pageSize, false, "")
+		domains, _, getErr := routingAPI.GetRoutingEmailDomains(pageSize, pageNum, false, "")
 		if getErr != nil {
 			return nil, diag.Errorf("Failed to get routing email domains: %v", getErr)
 		}
