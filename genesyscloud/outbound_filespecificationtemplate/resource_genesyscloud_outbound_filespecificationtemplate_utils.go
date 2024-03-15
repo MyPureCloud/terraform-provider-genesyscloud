30e946f92a87c0351e665daad9131711812f0dae
dginty4 <108797778+dginty4@users.noreply.github.com>
2024-03-15 12:52:53 +0000

    NO-JIRA: Upgrading go sdk (#915)
    * Upgraded go sdk
    
    * Upgrading go sdk
    
    * Update go.mod

--- genesyscloud/outbound_filespecificationtemplate/resource_genesyscloud_outbound_filespecificationtemplate_utils.go
+++ genesyscloud/outbound_filespecificationtemplate/resource_genesyscloud_outbound_filespecificationtemplate_utils.go
@@ -2,7 +2,7 @@ package outbound_filespecificationtemplate
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/mypurecloud/platform-client-sdk-go/v123/platformclientv2"
+	"github.com/mypurecloud/platform-client-sdk-go/v125/platformclientv2"
 	"terraform-provider-genesyscloud/genesyscloud/util/resourcedata"
 )
 
