f1a399110a2102670f6b47148514724a768465e4
carnellj-genesys <109529583+carnellj-genesys@users.noreply.github.com>
2024-03-19 10:54:51 -0400

    DEVTOOLING-455:  Added a wrapper to get all of the diagnostic logs un… (#909)
    * DEVTOOLING-455:  Added a wrapper to get all of the diagnostic logs under management
    
    * DEVTOOLING-455: Added some additional logging
    
    * DEVTOOLING-455: Cleaned up the code for the diagnostics work
    
    * DEVTOOLING-455: Cleaned up the code for the diagnostics work
    
    * DEVTOOLING-455: Cleaned up the code for the diagnostics work
    
    * DEVTOOLING-455: Cleaned up the code for the diagnostics work
    
    * DEVTOOLING-455: Added diag wrapper

---
+++ genesyscloud/util/util_diagnostics.go
@@ -0,0 +1,71 @@
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/mypurecloud/platform-client-sdk-go/v125/platformclientv2"
+)
+
+type detailedDiagnosticInfo struct {
+	ResourceName  string `json:"resourceName,omitempty"`
+	Method        string `json:"method,omitempty"`
+	Path          string `json:"path:omitempty"`
+	StatusCode    int    `json:"statusCode:omitempty"`
+	ErrorMessage  string `json:"errorMessage,omitempty"`
+	CorrelationID string `json:"correlationId,omitempty"`
+}
+
+func convertResponseToWrapper(resourceName string, apiResponse *platformclientv2.APIResponse) *detailedDiagnosticInfo {
+	return &detailedDiagnosticInfo{
+		ResourceName:  resourceName,
+		Method:        apiResponse.Response.Request.Method,
+		Path:          apiResponse.Response.Request.URL.Path,
+		StatusCode:    apiResponse.StatusCode,
+		ErrorMessage:  apiResponse.ErrorMessage,
+		CorrelationID: apiResponse.CorrelationID,
+	}
+}
+
+func BuildAPIDiagnosticError(resourceName string, summary string, apiResponse *platformclientv2.APIResponse) diag.Diagnostics {
+
+	//Checking to make sure we have properly formed response
+	if apiResponse == nil || apiResponse.Response == nil || apiResponse.Response.Request == nil || apiResponse.Response.Request.URL == nil {
+		error := fmt.Errorf("Unable to build a message from the response because the APIResponse does not contain the appropriate data.%s", "")
+		return BuildDiagnosticError(resourceName, summary, error)
+	}
+	diagInfo := convertResponseToWrapper(resourceName, apiResponse)
+	diagInfoByte, err := json.Marshal(diagInfo)
+
+	//Checking to see if we can Marshall the data
+	if err != nil {
+		error := fmt.Errorf("Unable to unmarshal diagnostic info while building diagnostic error. Erro: %s", err)
+		return BuildDiagnosticError(resourceName, summary, error)
+	}
+
+	dg := diag.Diagnostic{Severity: diag.Error, Summary: summary, Detail: string(diagInfoByte)}
+	var dgs diag.Diagnostics
+	dgs = append(dgs, dg)
+	return dgs
+}
+
+func BuildDiagnosticError(resourceName string, summary string, err error) diag.Diagnostics {
+	var msg string
+
+	diagInfo := &detailedDiagnosticInfo{
+		ResourceName: resourceName,
+		ErrorMessage: fmt.Sprintf("%s", err),
+	}
+	diagInfoByte, err := json.Marshal(diagInfo)
+
+	if err != nil {
+		msg = fmt.Sprintf("{'resourceName': '%s', 'details': 'Unable to unmarshal diagnostic info while building diagnostic error'}", resourceName)
+	} else {
+		msg = string(diagInfoByte)
+	}
+
+	dg := diag.Diagnostic{Severity: diag.Error, Summary: summary, Detail: msg}
+	var dgs diag.Diagnostics
+	dgs = append(dgs, dg)
+	return dgs
+}
