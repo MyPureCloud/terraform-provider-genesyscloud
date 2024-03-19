3df98240afb492f00fbf2542d5d1ecf38ef8dc85
Charlie Conneely <charlie.conneely@genesys.com>
2024-03-19 15:17:07 +0000

    Removing ref to old gc sdk version

--- genesyscloud/util/util_diagnostic_unit_test.go
+++ genesyscloud/util/util_diagnostic_unit_test.go
@@ -2,7 +2,7 @@ package util
 
 import (
 	"encoding/json"
-	"github.com/mypurecloud/platform-client-sdk-go/v123/platformclientv2"
+	"github.com/mypurecloud/platform-client-sdk-go/v125/platformclientv2"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/url"
