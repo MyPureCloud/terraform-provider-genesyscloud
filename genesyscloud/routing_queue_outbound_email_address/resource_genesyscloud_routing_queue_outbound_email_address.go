bda2cdbf4efe3901c399cabe0dc5a99601375aae
dginty4 <declan.ginty@genesys.com>
2024-03-28 13:57:37 +0000

    Made changes

--- genesyscloud/routing_queue_outbound_email_address/resource_genesyscloud_routing_queue_outbound_email_address.go
+++ genesyscloud/routing_queue_outbound_email_address/resource_genesyscloud_routing_queue_outbound_email_address.go
@@ -6,16 +6,40 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/mypurecloud/platform-client-sdk-go/v125/platformclientv2"
 	"log"
-	"strings"
 	consistencyChecker "terraform-provider-genesyscloud/genesyscloud/consistency_checker"
 	"terraform-provider-genesyscloud/genesyscloud/provider"
+	resourceExporter "terraform-provider-genesyscloud/genesyscloud/resource_exporter"
 	"terraform-provider-genesyscloud/genesyscloud/util"
+	"terraform-provider-genesyscloud/genesyscloud/util/resourcedata"
 )
+
+/*
+The resource_genesyscloud_routing_queue_outbound-email_address.go contains all the methods that perform the core logic for the resource.
+*/
+
+func getAllAuthRoutingQueueOutboundEmailAddress(ctx context.Context, clientConfig *platformclientv2.Configuration) (resourceExporter.ResourceIDMetaMap, diag.Diagnostics) {
+	resources := make(resourceExporter.ResourceIDMetaMap)
+	proxy := getRoutingQueueOutboundEmailAddressProxy(clientConfig)
+
+	queues, _, err := proxy.getAllRoutingQueues(ctx)
+	if err != nil {
+		return nil, diag.Errorf("failed to get routing queues outbound email address: %s", err)
+	}
+
+	for _, queue := range *queues {
+		if queue.OutboundEmailAddress != nil && *queue.OutboundEmailAddress != nil {
+			resources[*queue.Id] = &resourceExporter.ResourceMeta{Name: *queue.Id + "-email-address"}
+		}
+	}
+
+	return resources, nil
+}
 
 func createRoutingQueueOutboundEmailAddress(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	queueId := d.Get("queue_id").(string)
-	log.Printf("creating conditional group routing rules for queue %s", queueId)
+	log.Printf("creating outbound email address for queue %s", queueId)
 	d.SetId(queueId)
 
 	return updateRoutingQueueOutboundEmailAddress(ctx, d, meta)
@@ -24,28 +48,41 @@ func createRoutingQueueOutboundEmailAddress(ctx context.Context, d *schema.Resou
 func readRoutingQueueOutboundEmailAddress(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	sdkConfig := meta.(*provider.ProviderMeta).ClientConfig
 	proxy := getRoutingQueueOutboundEmailAddressProxy(sdkConfig)
-	queueId := strings.Split(d.Id(), "/")[0]
+	queueId := d.Id()
 
 	log.Printf("Reading routing queue %s outbound email address", queueId)
 	return util.WithRetriesForRead(ctx, d, func() *retry.RetryError {
-
+		queueEmailAddress, resp, getErr := proxy.getRoutingQueueOutboundEmailAddress(ctx, queueId)
 		if getErr != nil {
 			if util.IsStatus404(resp) {
-				return retry.RetryableError(fmt.Errorf("failed to read conditional group routing for queue %s: %s", queueId, getErr))
+				return retry.RetryableError(fmt.Errorf("failed to read outbound email address for queue %s: %s", queueId, getErr))
 			}
-			return retry.NonRetryableError(fmt.Errorf("failed to read conditional group routing for queue %s: %s", queueId, getErr))
+			return retry.NonRetryableError(fmt.Errorf("failed to read outbound email address for queue %s: %s", queueId, getErr))
 		}
 
 		cc := consistencyChecker.NewConsistencyCheck(ctx, d, meta, ResourceRoutingQueueOutboundEmailAddress())
 
 		_ = d.Set("queue_id", queueId)
+		resourcedata.SetNillableReference(d, "domain_id", queueEmailAddress.Domain)
+		resourcedata.SetNillableValue()
 
 		return cc.CheckState()
 	})
 }
 
 func updateRoutingQueueOutboundEmailAddress(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	return nil
+	sdkConfig := meta.(*provider.ProviderMeta).ClientConfig
+	proxy := getRoutingQueueOutboundEmailAddressProxy(sdkConfig)
+	queueId := d.Id()
+
+	log.Printf("updating outbound email address for queue %s", queueId)
+	_, _, err := proxy.updateRoutingQueueOutboundEmailAddress(ctx, queueId, &sdkRules)
+	if err != nil {
+		return diag.Errorf("failed to update outbound email address for queue %s: %s", queueId, err)
+	}
+	log.Printf("updated outbound email address for queue %s", queueId)
+
+	return readRoutingQueueOutboundEmailAddress(ctx, d, meta)
 }
 
 func deleteRoutingQueueOutboundEmailAddress(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
--- genesyscloud/routing_queue_outbound_email_address/resource_genesyscloud_routing_queue_outbound_email_address_schema.go
+++ genesyscloud/routing_queue_outbound_email_address/resource_genesyscloud_routing_queue_outbound_email_address_schema.go
@@ -3,6 +3,7 @@ package routing_queue_outbound_email_address
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"terraform-provider-genesyscloud/genesyscloud/provider"
+	resourceExporter "terraform-provider-genesyscloud/genesyscloud/resource_exporter"
 	registrar "terraform-provider-genesyscloud/genesyscloud/resource_register"
 )
 
@@ -11,7 +12,7 @@ const resourceName = "genesyscloud_routing_queue_outbound_email_address"
 // SetRegistrar registers all of the resources, datasources and exporters in the package
 func SetRegistrar(regInstance registrar.Registrar) {
 	regInstance.RegisterResource(resourceName, ResourceRoutingQueueOutboundEmailAddress())
-	//regInstance.RegisterExporter(resourceName, OutboundCampaignruleExporter())
+	regInstance.RegisterExporter(resourceName, OutboundRoutingQueueOutboundEmailAddressExporter())
 }
 
 func ResourceRoutingQueueOutboundEmailAddress() *schema.Resource {
@@ -31,27 +32,27 @@ func ResourceRoutingQueueOutboundEmailAddress() *schema.Resource {
 				Type:        schema.TypeString,
 				Required:    true,
 			},
-			"outbound_email_address": {
-				Description: "The outbound email address settings for the queue.",
-				Type:        schema.TypeList,
-				MinItems:    1,
-				MaxItems:    1,
+			"domain_id": {
+				Description: "Unique ID of the email domain. e.g. \"test.example.com\"",
+				Type:        schema.TypeString,
 				Required:    true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"domain_id": {
-							Description: "Unique ID of the email domain. e.g. \"test.example.com\"",
-							Type:        schema.TypeString,
-							Required:    true,
-						},
-						"route_id": {
-							Description: "Unique ID of the email route.",
-							Type:        schema.TypeString,
-							Required:    true,
-						},
-					},
-				},
 			},
+			"route_id": {
+				Description: "Unique ID of the email route.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+		},
+	}
+}
+
+func OutboundRoutingQueueOutboundEmailAddressExporter() *resourceExporter.ResourceExporter {
+	return &resourceExporter.ResourceExporter{
+		GetResourcesFunc: provider.GetAllWithPooledClient(getAllAuthRoutingQueueOutboundEmailAddress),
+		RefAttrs: map[string]*resourceExporter.RefAttrSettings{
+			"queue_id":  {RefType: "genesyscloud_routing_queue"},
+			"route_id":  {RefType: "genesyscloud_routing_email_route"},
+			"domain_id": {RefType: "genesyscloud_routing_email_domain"},
 		},
 	}
 }
