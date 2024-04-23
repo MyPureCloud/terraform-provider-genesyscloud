08d713f2aed60fa65a1d5c5f0befc05d030ad0f5
Monisha Padmavathi Ragavan <mpadmava@DWH3D21RYH-D.local>
2024-04-23 18:48:14 +0530

    fix tests

--- genesyscloud/architect_grammar/data_source_genesyscloud_architect_grammar_test.go
+++ genesyscloud/architect_grammar/data_source_genesyscloud_architect_grammar_test.go
@@ -2,14 +2,12 @@ package architect_grammar
 
 import (
 	"fmt"
-	"log"
 	"terraform-provider-genesyscloud/genesyscloud/provider"
 	"terraform-provider-genesyscloud/genesyscloud/util"
 	"testing"
 
 	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/mypurecloud/platform-client-sdk-go/v125/platformclientv2"
 )
 
 func TestAccDataSourceArchitectGrammar(t *testing.T) {
@@ -20,7 +18,6 @@ func TestAccDataSourceArchitectGrammar(t *testing.T) {
 		description     = "Sample description"
 	)
 
-	cleanupArchitectGrammar("GrammarArchitect")
 	resource.Test(t, resource.TestCase{
 		PreCheck:          func() { util.TestAccPreCheck(t) },
 		ProviderFactories: provider.GetProviderFactories(providerResources, providerDataSources),
@@ -52,32 +49,4 @@ func generateGrammarDataSource(
 		depends_on=[%s]
 	}
 	`, resourceID, name, dependsOnResource)
-}
-
-func cleanupArchitectGrammar(idPrefix string) {
-	architectApi := platformclientv2.NewArchitectApi()
-
-	for pageNum := 1; ; pageNum++ {
-		const pageSize = 100
-		architectGrammars, _, getErr := architectApi.GetArchitectGrammars(pageNum, pageSize, "", "", nil, idPrefix, "", "", false)
-		if getErr != nil {
-			log.Printf("failed to get architect grammar %s", getErr)
-			return
-		}
-
-		if architectGrammars.Entities == nil || len(*architectGrammars.Entities) == 0 {
-			break
-		}
-
-		for _, grammar := range *architectGrammars.Entities {
-			if grammar.Name != nil {
-				_, _, delErr := architectApi.DeleteArchitectGrammar(*grammar.Id)
-				if delErr != nil {
-					log.Printf("failed to delete architect grammar %s", delErr)
-					return
-				}
-				log.Printf("Deleted architect grammar %s (%s)", *grammar.Id, *grammar.Name)
-			}
-		}
-	}
 }
