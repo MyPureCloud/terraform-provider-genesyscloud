d2ed0a0b64957a62671aadbe383be4ec12cf1684
Shruti Suryawanshi <Shruti.Suryawanshi@genesys.com>
2024-06-06 10:32:58 -0400

    code for compression

--- genesyscloud/tfexporter/resource_genesyscloud_tf_export.go
+++ genesyscloud/tfexporter/resource_genesyscloud_tf_export.go
@@ -1,9 +1,13 @@
 package tfexporter
 
 import (
+	"archive/zip"
 	"context"
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"path"
 	"path/filepath"
 	gcloud "terraform-provider-genesyscloud/genesyscloud/validators"
 
@@ -11,10 +15,16 @@ import (
 
 	registrar "terraform-provider-genesyscloud/genesyscloud/resource_register"
 
+	"terraform-provider-genesyscloud/genesyscloud/tfexporter_state"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"terraform-provider-genesyscloud/genesyscloud/tfexporter_state"
 )
+
+type fileMeta struct {
+	Path  string
+	IsDir bool
+}
 
 func SetRegistrar(l registrar.Registrar) {
 	l.RegisterResource("genesyscloud_tf_export", ResourceTfExport())
@@ -149,6 +159,46 @@ func createTfExport(ctx context.Context, d *schema.ResourceData, meta interface{
 		}
 
 		d.SetId(gre.exportDirPath)
+		//read all the files
+		var files []fileMeta
+		err := filepath.Walk(gre.exportDirPath, func(path string, info os.FileInfo, err error) error {
+			files = append(files, fileMeta{Path: path, IsDir: info.IsDir()})
+			return nil
+		})
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		//create a zip
+		fmt.Println("creating zip archive...")
+		archive, err := os.Create("../archive.zip")
+		if err != nil {
+			panic(err)
+		}
+		defer archive.Close()
+		zipWriter := zip.NewWriter(archive)
+
+		for _, f := range files {
+			if !f.IsDir {
+				fPath := f.Path
+
+				w, err := zipWriter.Create(path.Base(fPath))
+				if err != nil {
+					log.Fatalln(err)
+				}
+
+				file, err := os.Open(f.Path)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				defer file.Close()
+
+				if _, err = io.Copy(w, file); err != nil {
+					log.Fatalln(err)
+				}
+			}
+		}
+		zipWriter.Close()
 		return nil
 	}
 
