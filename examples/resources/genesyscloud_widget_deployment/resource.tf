2e1d2f34e054e43f5f6b7374ea7c44706d64e9a3
Charlie Conneely <charlie.conneely@genesys.com>
2024-06-12 16:40:16 +0100

    updated example widget deployment resource

--- examples/resources/genesyscloud_widget_deployment/resource.tf
+++ examples/resources/genesyscloud_widget_deployment/resource.tf
@@ -5,9 +5,8 @@ resource "genesyscloud_widget_deployment" "mywidget" {
   client_type             = "v1"
   authentication_required = true
   disabled                = true
-  client_config {
-    authentication_url = "https://examplewebsite.com"
-    webchat_skin       = "modern-caret-skin"
+  third_party_client_config = {
+    foo = "bar"
   }
   allowed_domains = []
 }
