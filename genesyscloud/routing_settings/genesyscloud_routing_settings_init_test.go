576e175dec51a2239868a1ba7b440e0b89e766fe
Brian Moyles <157126600+BrianMoyles@users.noreply.github.com>
2024-06-20 11:58:08 +0100

    Feat/devtooling 187 - Refactor routing settings (#1089)
    * Refactored Routing Settings
    
    * Added missing attributes to schema
    
    * Removed data source
    
    * Made requested Changes
    
    * Fixing merge conflicts

---
+++ genesyscloud/routing_settings/genesyscloud_routing_settings_init_test.go
@@ -0,0 +1,40 @@
+package routing_settings
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"sync"
+	"testing"
+)
+
+// providerResources holds a map of all registered resources
+var providerResources map[string]*schema.Resource
+
+type registerTestInstance struct {
+	resourceMapMutex sync.RWMutex
+}
+
+// registerTestResources registers all resources used in the tests
+func (r *registerTestInstance) registerTestResources() {
+	r.resourceMapMutex.Lock()
+	defer r.resourceMapMutex.Unlock()
+
+	providerResources[resourceName] = ResourceRoutingSettings()
+}
+
+// initTestResources initializes all test resources and data sources.
+func initTestResources() {
+	providerResources = make(map[string]*schema.Resource)
+
+	regInstance := &registerTestInstance{}
+
+	regInstance.registerTestResources()
+}
+
+// TestMain is a "setup" function called by the testing framework when run the test
+func TestMain(m *testing.M) {
+	// Run setup function before starting the test suite for routing_settings package
+	initTestResources()
+
+	// Run the test suite for the routing_settings package
+	m.Run()
+}
