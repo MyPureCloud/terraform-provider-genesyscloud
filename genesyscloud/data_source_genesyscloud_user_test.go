45a2dce1febfadd74c2cdcb72b3283d13d1b7219
Monisha Padmavathi Ragavan <mpadmava@DWH3D21RYH-D.local>
2024-06-26 22:52:54 +0530

    Fix testcases

--- genesyscloud/data_source_genesyscloud_user_test.go
+++ genesyscloud/data_source_genesyscloud_user_test.go
@@ -6,12 +6,10 @@ import (
 	"terraform-provider-genesyscloud/genesyscloud/provider"
 	"terraform-provider-genesyscloud/genesyscloud/util"
 	"testing"
-	"time"
 
 	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/mypurecloud/platform-client-sdk-go/v131/platformclientv2"
 )
 
 func TestAccDataSourceUser(t *testing.T) {
@@ -89,46 +87,4 @@ func generateUserDataSource(
         depends_on=[%s]
 	}
 	`, resourceID, email, name, dependsOnResource)
-}
-
-func checkUserDeleted(id string) resource.TestCheckFunc {
-	log.Printf("Fetching user with ID: %s\n", id)
-	return func(s *terraform.State) error {
-		maxAttempts := 18
-		for i := 0; i < maxAttempts; i++ {
-
-			deleted, err := isUserDeleted(id)
-			if err != nil {
-				return err
-			}
-			if deleted {
-				return nil
-			}
-			time.Sleep(10 * time.Second)
-		}
-		return fmt.Errorf("user %s was not deleted properly", id)
-	}
-}
-
-func isUserDeleted(id string) (bool, error) {
-	mu.Lock()
-	defer mu.Unlock()
-
-	usersAPI := platformclientv2.NewUsersApiWithConfig(sdkConfig)
-	// Attempt to get the user
-	_, response, err := usersAPI.GetUser(id, nil, "", "")
-
-	// Check if the user is not found (deleted)
-	if response != nil && response.StatusCode == 404 {
-		return true, nil // User is deleted
-	}
-
-	// Handle other errors
-	if err != nil {
-		log.Printf("Error fetching user: %v", err)
-		return false, err
-	}
-
-	// If user is found, it means the user is not deleted
-	return false, nil
 }
