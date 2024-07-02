fb72a00529de62ba35c9b305df081b89a2e35a25
Brian Goad <bdgoad@gmail.com>
2024-07-02 06:16:07 -0400

    Update latest language codes (#1130)
    * Pull in latest language codes from the API schema. See values available in dropdown for language field in API Explorer https://apicentral.genesys.cloud/api-explorer\#post-api-v2-knowledge-knowledgebases and https://apicentral.genesys.cloud/api-explorer\#post-api-v2-knowledge-knowledgebases--knowledgeBaseId--languages--languageCode--categories
    
    * Pull the language code logic out into its own validator function

--- genesyscloud/data_source_genesyscloud_knowledge_knowledgebase.go
+++ genesyscloud/data_source_genesyscloud_knowledge_knowledgebase.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 	"terraform-provider-genesyscloud/genesyscloud/provider"
 	"terraform-provider-genesyscloud/genesyscloud/util"
+	"terraform-provider-genesyscloud/genesyscloud/validators"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/mypurecloud/platform-client-sdk-go/v133/platformclientv2"
 )
 
@@ -26,10 +26,10 @@ func dataSourceKnowledgeKnowledgebase() *schema.Resource {
 				Required:    true,
 			},
 			"core_language": {
-				Description:  "Core language for knowledge base in which initial content must be created, language codes [en-US, en-UK, en-AU, de-DE] are supported currently, however the new DX knowledge will support all these language codes",
-				Type:         schema.TypeString,
-				Required:     true,
-				ValidateFunc: validation.StringInSlice([]string{"en-US", "en-UK", "en-AU", "de-DE", "es-US", "es-ES", "fr-FR", "pt-BR", "nl-NL", "it-IT", "fr-CA"}, false),
+				Description:      "Core language for knowledge base in which initial content must be created, language codes [en-US, en-UK, en-AU, de-DE] are supported currently, however the new DX knowledge will support all these language codes",
+				Type:             schema.TypeString,
+				Required:         true,
+				ValidateDiagFunc: validators.ValidateLanguageCode,
 			},
 		},
 	}
