aa2c81a7542a31c7e30d95d424f3b94ab8189aa6
Charlie Conneely <charlie.conneely@genesys.com>
2024-07-17 10:13:45 +0100

    Fixed journey action maps

--- test/data/resource/genesyscloud_journey_action_map/basic_optional_attributes/06_update_remove_end_date.tf
+++ test/data/resource/genesyscloud_journey_action_map/basic_optional_attributes/06_update_remove_end_date.tf
@@ -15,9 +15,6 @@ resource "genesyscloud_journey_action_map" "terraform_test_-TEST-CASE-" {
     max_quantile_threshold      = 0.666
     fallback_quantile_threshold = 0.125
   }
-  # optional
-
-  depends_on = [genesyscloud_journey_segment.terraform_test_-TEST-CASE-_action_map_dependency]
 }
 
 resource "genesyscloud_journey_segment" "terraform_test_-TEST-CASE-_action_map_dependency" {
@@ -25,6 +22,26 @@ resource "genesyscloud_journey_segment" "terraform_test_-TEST-CASE-_action_map_d
   color                   = "#008000"
   scope                   = "Session"
   should_display_to_agent = false
+  journey {
+    patterns {
+      criteria {
+        key                = "page.title"
+        values             = ["Title"]
+        operator           = "notEqual"
+        should_ignore_case = true
+      }
+      count        = 1
+      stream_type  = "Web"
+      session_type = "web"
+    }
+  }
+}
+
+resource "genesyscloud_journey_outcome" "terraform_test_-TEST-CASE-_action_map_dependency" {
+  is_active    = true
+  display_name = "terraform_test_-TEST-CASE-_action_map_dependency"
+  description  = "test description of journey outcome"
+  is_positive  = true
   journey {
     patterns {
       criteria {
