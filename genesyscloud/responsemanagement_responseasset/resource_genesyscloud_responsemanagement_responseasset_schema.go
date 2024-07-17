1a6fec10cebd6d07dbfe027779e2d259a9fe4605
dginty4 <declan.ginty@genesys.com>
2024-07-17 11:46:05 +0100

    Added reponse assets stuff back

--- genesyscloud/responsemanagement_responseasset/resource_genesyscloud_responsemanagement_responseasset_schema.go
+++ genesyscloud/responsemanagement_responseasset/resource_genesyscloud_responsemanagement_responseasset_schema.go
@@ -5,6 +5,7 @@ import (
 	"terraform-provider-genesyscloud/genesyscloud/provider"
 	resourceExporter "terraform-provider-genesyscloud/genesyscloud/resource_exporter"
 	registrar "terraform-provider-genesyscloud/genesyscloud/resource_register"
+	"terraform-provider-genesyscloud/genesyscloud/validators"
 )
 
 /*
@@ -39,11 +40,11 @@ func ResourceResponseManagementResponseAsset() *schema.Resource {
 		SchemaVersion: 1,
 		Schema: map[string]*schema.Schema{
 			`filename`: {
-				Description: "Name of the file to upload. Changing the name attribute will cause the existing response asset to be dropped and recreated with a new ID. It must not start with a dot and not end with a forward slash. Whitespace and the following characters are not allowed: \\{^}%`]\">[~<#|",
-				Required:    true,
-				ForceNew:    true,
-				Type:        schema.TypeString,
-				//ValidateDiagFunc: validators.ValidateResponseAssetName,
+				Description:      "Name of the file to upload. Changing the name attribute will cause the existing response asset to be dropped and recreated with a new ID. It must not start with a dot and not end with a forward slash. Whitespace and the following characters are not allowed: \\{^}%`]\">[~<#|",
+				Required:         true,
+				ForceNew:         true,
+				Type:             schema.TypeString,
+				ValidateDiagFunc: validators.ValidateResponseAssetName,
 			},
 			`division_id`: {
 				Description: `Division to associate to this asset. Can only be used with this division.`,
