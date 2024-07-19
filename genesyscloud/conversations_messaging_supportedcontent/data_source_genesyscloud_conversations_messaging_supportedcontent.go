d0e6c75d9df49d11fdd0a8df167304538067de48
Shruti Suryawanshi <Shruti.Suryawanshi@genesys.com>
2024-07-19 16:35:43 -0400

    changed resource file names

---
+++ genesyscloud/conversations_messaging_supportedcontent/data_source_genesyscloud_conversations_messaging_supportedcontent.go
@@ -0,0 +1,43 @@
+package supported_content
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+
+	"terraform-provider-genesyscloud/genesyscloud/provider"
+	"terraform-provider-genesyscloud/genesyscloud/util"
+)
+
+/*
+   The data_source_genesyscloud_conversations_messaging_supportedcontent.go contains the data source implementation
+   for the resource.
+*/
+
+// dataSourceSupportedContentRead retrieves by name the id in question
+func dataSourceSupportedContentRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	sdkConfig := meta.(*provider.ProviderMeta).ClientConfig
+	proxy := getSupportedContentProxy(sdkConfig)
+
+	name := d.Get("name").(string)
+
+	return util.WithRetries(ctx, 15*time.Second, func() *retry.RetryError {
+		supportedContentId, retryable, resp, err := proxy.getSupportedContentIdByName(ctx, name)
+
+		if err != nil && !retryable {
+			retry.NonRetryableError(util.BuildWithRetriesApiDiagnosticError(resourceName, fmt.Sprintf("Error searching supported content %s: %s", name, err), resp))
+		}
+
+		if retryable {
+			retry.RetryableError(util.BuildWithRetriesApiDiagnosticError(resourceName, fmt.Sprintf("No supported content found with name %s", name), resp))
+		}
+
+		d.SetId(supportedContentId)
+		return nil
+	})
+}
---
+++ genesyscloud/conversations_messaging_supportedcontent/data_source_genesyscloud_conversations_messaging_supportedcontent_test.go
@@ -0,0 +1,62 @@
+package supported_content
+
+import (
+	"fmt"
+	"testing"
+
+	"terraform-provider-genesyscloud/genesyscloud/provider"
+	"terraform-provider-genesyscloud/genesyscloud/util"
+
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+/*
+Test Class for the supported content Data Source
+*/
+
+func TestAccDataSourceSupportedContent(t *testing.T) {
+	t.Parallel()
+	var (
+		resourceId   = "testSupportedContent"
+		dataSourceId = "testSupportedContent_data"
+		name         = "Terraform Supported Content - " + uuid.NewString()
+		inboundType  = "*/*"
+		outboundType = "image/*"
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { util.TestAccPreCheck(t) },
+		ProviderFactories: provider.GetProviderFactories(providerResources, providerDataSources),
+		Steps: []resource.TestStep{
+			{
+				Config: GenerateSupportedContentResource(
+					resourceId,
+					name,
+					GenerateInboundTypeBlock(inboundType),
+					GenerateOutboundTypeBlock(outboundType),
+				) +
+					GenerateDataSourceForSupportedContent(
+						dataSourceId,
+						name,
+						"genesyscloud_conversations_messaging_supportedcontent."+resourceId),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair("data.genesyscloud_conversations_messaging_supportedcontent."+dataSourceId, "id", "genesyscloud_conversations_messaging_supportedcontent."+resourceId, "id"),
+				),
+			},
+		},
+	})
+}
+
+func GenerateDataSourceForSupportedContent(
+	resourceId string,
+	name string,
+	dependsOn string,
+) string {
+	return fmt.Sprintf(`
+	data "`+resourceName+`" "%s" {
+		name = "%s"
+		depends_on = [%s]
+	}
+	`, resourceId, name, dependsOn)
+}
