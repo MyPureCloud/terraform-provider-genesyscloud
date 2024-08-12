9d94f5d821bb8748a6e9cb3320eaea9351f1ff01
Shruti Suryawanshi <Shruti.Suryawanshi@genesys.com>
2024-08-12 18:02:56 -0400

    updated docs

--- examples/resources/genesyscloud_conversations_messaging_integrations_instagram/resource.tf
+++ examples/resources/genesyscloud_conversations_messaging_integrations_instagram/resource.tf
@@ -2,6 +2,6 @@ resource "genesyscloud_conversations_messaging_integrations_instagram" "test_sam
   name                 = "Test Integration Instagram"
   user_access_token    = "1234567890"
   page_id              = "1"
-  messaging_setting_id = "2c4e3b8e-3c9f-45c9-82cd-4bb54c8f18f0"
-  supported_content_id = "019c37a7-ccb4-4966-b1d7-ddb20399f7ab"
+  messaging_setting_id = genesyscloud_conversations_messaging_settings.settings.id
+  supported_content_id = genesyscloud_conversations_messaging_supportedcontent.content.id
 }
