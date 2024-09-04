ea164db219c7a8d751fa68d9e511c12879ba9590
John Carnell <john.carnell@genesys.com>
2024-09-04 15:13:56 -0400

    NO-JIRA: Adding new python code to generate shell

--- .devcontainer/generate_env.py
+++ .devcontainer/generate_env.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
-
 import os
 
 def get_user_input(prompt):
     return input(prompt).strip()
+
+
 
 def generate_shell_script(client_id, client_secret, region):
     script_content = f"""#!/bin/bash
@@ -16,6 +16,15 @@ echo "Environment variables set successfully!"
 """
     return script_content
 
+
+def generate_cli_config(client_id, client_secret, region):
+    script_content = f"""[default]
+client_credentials = '{client_id}'
+client_secret = '{client_secret}'
+environment = 'usw2.pure.cloud'
+"""
+    return script_content
+
 def main():
     print("Please enter the following values to generate the environment variables needed to run terraform against your target environment: ")
     
@@ -24,6 +33,7 @@ def main():
     region = get_user_input("GENESYSCLOUD_REGION: ")
 
     script_content = generate_shell_script(client_id, client_secret, region)
+    cli_content
 
     script_filename = "set_genesys_env.sh"
     with open(script_filename, "w") as f:
@@ -32,7 +42,7 @@ def main():
     print(f"\nShell script '{script_filename}' has been generated.")
     print(f"To use it, run: source {script_filename}")
 
-    # Make the script executable
+    #Make the script executable
     os.chmod(script_filename, 0o755)
 
 if __name__ == "__main__":
