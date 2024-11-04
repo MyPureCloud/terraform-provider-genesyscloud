c60ee6fd3c3297b89f7c7c0fa0dd0d14e2ab962f
Charlie Conneely <87772562+charliecon@users.noreply.github.com>
2024-11-04 19:26:45 +0000

    Fixing incorrect resource name in knowledge_label example (#1326)

--- examples/resources/genesyscloud_knowledge_label/resource.tf
+++ examples/resources/genesyscloud_knowledge_label/resource.tf
@@ -1,4 +1,4 @@
-resource "genesyscloud_knowledge_category_v1" "example_category" {
+resource "genesyscloud_knowledge_label" "label" {
   knowledge_base_id = genesyscloud_knowledge_knowledgebase.example_knowledgebase.id
   knowledge_label {
     name  = "ExampleLabel"
