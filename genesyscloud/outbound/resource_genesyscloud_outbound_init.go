db7437d60d6d17e3e1a095f3622eecffd2efe658
Charlie Conneely <charlie.conneely@genesys.com>
2024-11-08 10:40:04 +0000

    Deleted leftover file

--- genesyscloud/outbound/resource_genesyscloud_outbound_init.go
+++ genesyscloud/outbound/resource_genesyscloud_outbound_init.go
@@ -5,8 +5,6 @@ import (
 )
 
 func SetRegistrar(l registrar.Registrar) {
-	l.RegisterDataSource("genesyscloud_outbound_attempt_limit", DataSourceOutboundAttemptLimit())
-
 	l.RegisterDataSource("genesyscloud_outbound_messagingcampaign", dataSourceOutboundMessagingcampaign())
 	l.RegisterResource("genesyscloud_outbound_messagingcampaign", ResourceOutboundMessagingCampaign())
 	l.RegisterExporter("genesyscloud_outbound_messagingcampaign", OutboundMessagingcampaignExporter())
