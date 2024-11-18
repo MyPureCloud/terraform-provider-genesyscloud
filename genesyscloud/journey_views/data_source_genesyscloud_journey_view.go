b5bead177b7d38698cb4fb0e84c3465e70fd4002
llegouez <60443674+llegouez@users.noreply.github.com>
2024-11-18 18:45:00 +0100

    JM-1465 - add JourneyViews exporter and datasource (#1355)
    * JM-1465
    
    * Test
    
    * Fix datasource
    
    * fix exporter
    
    * JM-1465 fix ds test
    
    * JM-1465 fix naming
    
    * JM-1465 fix tests
    
    * JM-1465 fix tests
    
    * JM-1465 fix ds tests
    
    * remove unwanted changes
    
    * Fix journeyview cache
    
    * rollback unwanted changes
    
    * get journey check cache
    
    * fix PR comments
    
    * fix retryable error
    
    * fix printf
    
    ---------
    
    Co-authored-by: Nicolas Bertrand <nicolas.bertrand@genesys.com>

---
+++ genesyscloud/journey_views/data_source_genesyscloud_journey_view.go
@@ -0,0 +1,49 @@
+package journey_views
+
+import (
+	"context"
+	"fmt"
+	"terraform-provider-genesyscloud/genesyscloud/provider"
+	"terraform-provider-genesyscloud/genesyscloud/util"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceJourneyViewRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	name := d.Get("name").(string)
+
+	journeyId, err := getJourneyByNameFn(name, ctx, m)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(journeyId)
+	return nil
+}
+
+// getJourneyByNameFn returns the journey id (blank if not found) and diag
+func getJourneyByNameFn(name string, ctx context.Context, m interface{}) (string, diag.Diagnostics) {
+	config := m.(*provider.ProviderMeta).ClientConfig
+	proxy := getJourneyViewProxy(config)
+	journeyId := ""
+
+	diag := util.WithRetries(ctx, 15*time.Second, func() *retry.RetryError {
+		foundId, resp, getErr, retryable := proxy.getJourneyViewByName(ctx, name)
+		if getErr != nil {
+			if retryable {
+				return retry.RetryableError(util.BuildWithRetriesApiDiagnosticError(name, fmt.Sprintf("No journey view found with search %s", name), resp))
+			} else {
+				return retry.NonRetryableError(util.BuildWithRetriesApiDiagnosticError(name, fmt.Sprintf("Error requesting journey view %s | error: %s", name, getErr), resp))
+			}
+		}
+
+		journeyId = foundId
+		return nil
+	})
+
+	return journeyId, diag
+}
--- genesyscloud/journey_views/resource_genesyscloud_journey_views_test.go
+++ genesyscloud/journey_views/resource_genesyscloud_journey_views_test.go
@@ -7,7 +7,6 @@ import (
 	"terraform-provider-genesyscloud/genesyscloud/util"
 	"testing"
 
-	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/mypurecloud/platform-client-sdk-go/v143/platformclientv2"
@@ -27,9 +26,6 @@ func TestAccResourceJourneyViewsBasic(t *testing.T) {
 		predicatesValues    = "VOICE"
 		predicatesOperator  = "Matches"
 		predicatesNoValue   = false
-		testUserResource    = "user_resource1"
-		testUserName        = "nameUser1" + uuid.NewString()
-		testUserEmail       = uuid.NewString() + "@example.com"
 		journeyResource     = "journey_resource1"
 		emptyElementBlock   = ""
 	)
@@ -39,7 +35,7 @@ func TestAccResourceJourneyViewsBasic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				//Create
-				Config: generateUserWithCustomAttrs(testUserResource, testUserEmail, testUserName) + generateJourneyView(journeyResource, name, duration, emptyElementBlock),
+				Config: generateJourneyView(journeyResource, name, duration, emptyElementBlock),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "name", name),
 					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "duration", duration),
@@ -47,7 +43,7 @@ func TestAccResourceJourneyViewsBasic(t *testing.T) {
 			},
 			{
 				//Update
-				Config: generateUserWithCustomAttrs(testUserResource, testUserEmail, testUserName) + generateJourneyView(journeyResource, name, duration, generateElements(
+				Config: generateJourneyView(journeyResource, name, duration, generateElements(
 					elementsId,
 					elementsName,
 					generateAttributes(attributeType, attributeId, attributeSource),
@@ -74,7 +70,7 @@ func TestAccResourceJourneyViewsBasic(t *testing.T) {
 			},
 			{
 				//Update without filter
-				Config: generateUserWithCustomAttrs(testUserResource, testUserEmail, testUserName) + generateJourneyView(journeyResource, name, duration, generateElements(
+				Config: generateJourneyView(journeyResource, name, duration, generateElements(
 					elementsId,
 					elementsName,
 					generateAttributes(attributeType, attributeId, attributeSource),
@@ -117,7 +113,8 @@ func generateJourneyView(journeyResource string, name string, duration string, e
     duration = "%s"
     name = "%s"
     %s
-}`, journeyResource, duration, name, func() string {
+	}
+	`, journeyResource, duration, name, func() string {
 		if elementsBlock != "" {
 			return elementsBlock
 		}
@@ -132,7 +129,8 @@ func generateElements(id string, name string, attributesBlock string, filter str
         name = "%s"
         %s
         %s
-    }`, id, name, attributesBlock, filter)
+    }
+    `, id, name, attributesBlock, filter)
 }
 
 func generateFilter(filterType string, nestedBlocks ...string) string {
@@ -140,7 +138,8 @@ func generateFilter(filterType string, nestedBlocks ...string) string {
         filter {
             type       = "%s"
             %s
-        }`, filterType, strings.Join(nestedBlocks, "\n"))
+        }
+        `, filterType, strings.Join(nestedBlocks, "\n"))
 }
 
 func generateAttributes(attributeType string, attributeId string, attributeSource string) string {
@@ -149,7 +148,8 @@ func generateAttributes(attributeType string, attributeId string, attributeSourc
             type   = "%s"
             id     = "%s"
             source = "%s"
-        }`, attributeType, attributeId, attributeSource)
+        }
+        `, attributeType, attributeId, attributeSource)
 }
 
 func generatePredicates(dimension string, values string, operator string, noValue bool) string {
@@ -159,7 +159,8 @@ func generatePredicates(dimension string, values string, operator string, noValu
                 values    = ["%s"]
                 operator  = "%s"
                 no_value  = %v
-            }`, dimension, values, operator, noValue)
+            }
+            `, dimension, values, operator, noValue)
 }
 
 func testVerifyJourneyViewsDestroyed(state *terraform.State) error {
