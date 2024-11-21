3b7d487c18da592065578c91b88e58564542e791
Charlie Conneely <87772562+charliecon@users.noreply.github.com>
2024-11-21 09:30:50 +0000

    Revert "WIP: JM-1645: Use Put about Update and add Charts (#1371)"
    This reverts commit c038cce4a6fc54155b0b1223ed38142b6b1bc71e.

--- genesyscloud/journey_views/resource_genesyscloud_journey_views_test.go
+++ genesyscloud/journey_views/resource_genesyscloud_journey_views_test.go
@@ -15,7 +15,6 @@ import (
 func TestAccResourceJourneyViewsBasic(t *testing.T) {
 	var (
 		name                = "test journey from tf 1"
-		nameUpdated         = "test journey from tf 1 updated"
 		duration            = "P1Y"
 		elementsId          = "ac6c61b5-1cd4-4c6e-a8a5-edb74d9117eb"
 		elementsName        = "Wrap Up"
@@ -28,11 +27,7 @@ func TestAccResourceJourneyViewsBasic(t *testing.T) {
 		predicatesOperator  = "Matches"
 		predicatesNoValue   = false
 		journeyResource     = "journey_resource1"
-		chartName           = "Chart 1"
-		chartVersion        = 1
-		metricId            = "Metric 1"
-		metricDisplayLabel  = "Display Label"
-		metricAggregate     = "CustomerCount"
+		emptyElementBlock   = ""
 	)
 	resource.Test(t, resource.TestCase{
 		PreCheck:          func() { util.TestAccPreCheck(t) },
@@ -40,12 +35,20 @@ func TestAccResourceJourneyViewsBasic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				//Create
+				Config: generateJourneyView(journeyResource, name, duration, emptyElementBlock),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "name", name),
+					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "duration", duration),
+				),
+			},
+			{
+				//Update
 				Config: generateJourneyView(journeyResource, name, duration, generateElements(
 					elementsId,
 					elementsName,
 					generateAttributes(attributeType, attributeId, attributeSource),
 					generateFilter(filterType, generatePredicates(predicatesDimension, predicatesValues, predicatesOperator, predicatesNoValue)),
-				), generateCharts(chartName, chartVersion, generateMetrics(metricId, elementsId, metricAggregate, metricDisplayLabel))),
+				)),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "name", name),
 					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "duration", duration),
@@ -63,26 +66,18 @@ func TestAccResourceJourneyViewsBasic(t *testing.T) {
 					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.filter.0.predicates.0.values.0", predicatesValues),
 					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.filter.0.predicates.0.operator", predicatesOperator),
 					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.filter.0.predicates.0.no_value", fmt.Sprintf("%t", predicatesNoValue)),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.#", "1"),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.0.name", chartName),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.0.version", fmt.Sprintf("%v", chartVersion)),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.0.metrics.#", "1"),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.0.metrics.0.id", metricId),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.0.metrics.0.display_label", metricDisplayLabel),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.0.metrics.0.aggregate", metricAggregate),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.0.metrics.0.element_id", elementsId),
 				),
 			},
 			{
-				//Create
-				Config: generateJourneyView(journeyResource, nameUpdated, duration, generateElements(
+				//Update without filter
+				Config: generateJourneyView(journeyResource, name, duration, generateElements(
 					elementsId,
 					elementsName,
 					generateAttributes(attributeType, attributeId, attributeSource),
-					generateFilter(filterType, generatePredicates(predicatesDimension, predicatesValues, predicatesOperator, predicatesNoValue)),
-				), generateCharts(chartName, chartVersion, generateMetrics(metricId, elementsId, metricAggregate, metricDisplayLabel))),
+					"",
+				)),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "name", nameUpdated),
+					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "name", name),
 					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "duration", duration),
 					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.id", elementsId),
 					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.name", elementsName),
@@ -90,22 +85,7 @@ func TestAccResourceJourneyViewsBasic(t *testing.T) {
 					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.attributes.0.type", attributeType),
 					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.attributes.0.id", attributeId),
 					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.attributes.0.source", attributeSource),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.filter.#", "1"),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.filter.0.type", "And"),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.filter.0.predicates.#", "1"),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.filter.0.predicates.0.dimension", predicatesDimension),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.filter.0.predicates.0.values.#", "1"),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.filter.0.predicates.0.values.0", predicatesValues),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.filter.0.predicates.0.operator", predicatesOperator),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.filter.0.predicates.0.no_value", fmt.Sprintf("%t", predicatesNoValue)),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.#", "1"),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.0.name", chartName),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.0.version", fmt.Sprintf("%v", chartVersion)),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.0.metrics.#", "1"),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.0.metrics.0.id", metricId),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.0.metrics.0.display_label", metricDisplayLabel),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.0.metrics.0.aggregate", metricAggregate),
-					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "charts.0.metrics.0.element_id", elementsId),
+					resource.TestCheckResourceAttr("genesyscloud_journey_views."+journeyResource, "elements.0.filter.#", "0"),
 				),
 			},
 			{
@@ -128,25 +108,18 @@ func generateUserWithCustomAttrs(resourceID string, email string, name string, a
 	`, resourceID, email, name, strings.Join(attrs, "\n"))
 }
 
-func generateJourneyView(journeyResource string, name string, duration string, elementsBlock string, chartsBlock string) string {
+func generateJourneyView(journeyResource string, name string, duration string, elementsBlock string) string {
 	return fmt.Sprintf(`resource "genesyscloud_journey_views" "%s" {
     duration = "%s"
     name = "%s"
     %s
-	%s
 	}
 	`, journeyResource, duration, name, func() string {
 		if elementsBlock != "" {
 			return elementsBlock
 		}
 		return ""
-	}(),
-		func() string {
-			if chartsBlock != "" {
-				return chartsBlock
-			}
-			return ""
-		}())
+	}())
 }
 
 func generateElements(id string, name string, attributesBlock string, filter string) string {
@@ -188,27 +161,6 @@ func generatePredicates(dimension string, values string, operator string, noValu
                 no_value  = %v
             }
             `, dimension, values, operator, noValue)
-}
-
-func generateCharts(name string, version int, metricsBlock string) string {
-	return fmt.Sprintf(`
-    charts {
-        name = "%s"
-        version = %d
-        %s
-    }
-    `, name, version, metricsBlock)
-}
-
-func generateMetrics(id string, elementId string, aggregate string, displayLabel string) string {
-	return fmt.Sprintf(`
-        metrics {
-            id = "%s"
-            element_id = "%s"
-            aggregate = "%s"
-            display_label = "%s"
-        }
-        `, id, elementId, aggregate, displayLabel)
 }
 
 func testVerifyJourneyViewsDestroyed(state *terraform.State) error {
