33f94933288e4570b8bc6a8f4fa72d925ca22790
kavinstanes96 <kavin_arasu.balasubramanian@genesys.com>
2024-12-02 08:58:50 -0500

    added proxy to data source

--- genesyscloud/knowledge_category/data_source_genesyscloud_knowledge_category.go
+++ genesyscloud/knowledge_category/data_source_genesyscloud_knowledge_category.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/mypurecloud/platform-client-sdk-go/v146/platformclientv2"
 )
 
 func dataSourceKnowledgeCategory() *schema.Resource {
@@ -35,66 +34,24 @@ func dataSourceKnowledgeCategory() *schema.Resource {
 
 func dataSourceKnowledgeCategoryRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	sdkConfig := m.(*provider.ProviderMeta).ClientConfig
-	knowledgeAPI := platformclientv2.NewKnowledgeApiWithConfig(sdkConfig)
-
+	proxy := GetKnowledgeCategoryProxy(sdkConfig)
 	name := d.Get("name").(string)
 	knowledgeBaseName := d.Get("knowledge_base_name").(string)
 
-	return util.WithRetries(ctx, 15*time.Second, func() *retry.RetryError {
-		const pageSize = 100
-		publishedKnowledgeBases, publishedResp, getPublishedErr := knowledgeAPI.GetKnowledgeKnowledgebases("", "", "", fmt.Sprintf("%v", pageSize), knowledgeBaseName, "", true, "", "")
-		unpublishedKnowledgeBases, unpublishedResp, getUnpublishedErr := knowledgeAPI.GetKnowledgeKnowledgebases("", "", "", fmt.Sprintf("%v", pageSize), knowledgeBaseName, "", false, "", "")
+	return util.WithRetries(ctx, 10*time.Second, func() *retry.RetryError {
+		knowledgeCategoryId, retryable, resp, getErr := proxy.getKnowledgeCategoryByName(ctx, name, knowledgeBaseName)
 
-		if getPublishedErr != nil {
-			return retry.NonRetryableError(util.BuildWithRetriesApiDiagnosticError("genesyscloud_knowledge_category", fmt.Sprintf("Failed to get knowledge base %s | error: %s", knowledgeBaseName, getPublishedErr), publishedResp))
-		}
-		if getUnpublishedErr != nil {
-			return retry.NonRetryableError(util.BuildWithRetriesApiDiagnosticError("genesyscloud_knowledge_category", fmt.Sprintf("Failed to get knowledge base %s | error: %s", knowledgeBaseName, getUnpublishedErr), unpublishedResp))
-		}
-
-		noPublishedEntities := publishedKnowledgeBases.Entities == nil || len(*publishedKnowledgeBases.Entities) == 0
-		noUnpublishedEntities := unpublishedKnowledgeBases.Entities == nil || len(*unpublishedKnowledgeBases.Entities) == 0
-		if noPublishedEntities && noUnpublishedEntities {
-
-			return retry.RetryableError(util.BuildWithRetriesApiDiagnosticError("genesyscloud_knowledge_category", fmt.Sprintf("no knowledge bases found with name %s", knowledgeBaseName), publishedResp))
-		}
-
-		// prefer published knowledge base
-		for _, knowledgeBase := range *publishedKnowledgeBases.Entities {
-			if knowledgeBase.Name != nil && *knowledgeBase.Name == knowledgeBaseName {
-				knowledgeCategories, resp, getErr := knowledgeAPI.GetKnowledgeKnowledgebaseCategories(*knowledgeBase.Id, "", "", fmt.Sprintf("%v", pageSize), "", false, name, "", "", false)
-
-				if getErr != nil {
-					return retry.NonRetryableError(util.BuildWithRetriesApiDiagnosticError("genesyscloud_knowledge_category", fmt.Sprintf("Failed to get knowledge category %s | error: %s", name, getErr), resp))
-				}
-
-				for _, knowledgeCategory := range *knowledgeCategories.Entities {
-					if *knowledgeCategory.Name == name {
-						id := fmt.Sprintf("%s,%s", *knowledgeCategory.Id, *knowledgeCategory.KnowledgeBase.Id)
-						d.SetId(id)
-						return nil
-					}
-				}
+		if getErr != nil {
+			errMsg := util.BuildWithRetriesApiDiagnosticError("genesyscloud_knowledge_category", fmt.Sprintf("Failed to get knowledge category by name %s | error: %s", name, getErr), resp)
+			if !retryable {
+				return retry.NonRetryableError(errMsg)
 			}
-		}
-		// use unpublished knowledge base if unpublished doesn't exist
-		for _, knowledgeBase := range *unpublishedKnowledgeBases.Entities {
-			if knowledgeBase.Name != nil && *knowledgeBase.Name == knowledgeBaseName {
-				knowledgeCategories, resp, getErr := knowledgeAPI.GetKnowledgeKnowledgebaseCategories(*knowledgeBase.Id, "", "", fmt.Sprintf("%v", pageSize), "", false, name, "", "", false)
+			return retry.RetryableError(errMsg)
 
-				if getErr != nil {
-					return retry.NonRetryableError(util.BuildWithRetriesApiDiagnosticError("genesyscloud_knowledge_category", fmt.Sprintf("Failed to get knowledge category %s | error: %s", name, getErr), resp))
-				}
-
-				for _, knowledgeCategory := range *knowledgeCategories.Entities {
-					if *knowledgeCategory.Name == name {
-						id := fmt.Sprintf("%s,%s", *knowledgeCategory.Id, *knowledgeCategory.KnowledgeBase.Id)
-						d.SetId(id)
-						return nil
-					}
-				}
-			}
 		}
+
+		d.SetId(knowledgeCategoryId)
+
 		return nil
 	})
 }
