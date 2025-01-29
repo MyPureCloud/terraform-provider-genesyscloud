3fc3a8b56500f75db8bff544112aabc75350cf5e
Charlie Conneely <charlie.conneely@genesys.com>
2025-01-29 17:52:47 +0000

    Moved function to break cyclic dependency

--- genesyscloud/util/panic_recovery_logger/panic_recovery_logger.go
+++ genesyscloud/util/panic_recovery_logger/panic_recovery_logger.go
@@ -2,8 +2,8 @@ package panic_recovery_logger
 
 import (
 	"fmt"
+	"os"
 	"runtime/debug"
-	"terraform-provider-genesyscloud/genesyscloud/util/files"
 )
 
 type PanicRecoveryLogger struct {
@@ -31,8 +31,33 @@ func GetPanicRecoveryLoggerInstance() *PanicRecoveryLogger {
 
 func (p *PanicRecoveryLogger) WriteStackTracesToFile(r any) error {
 	tracesToWrite := fmt.Sprintf("\nStacktrace recovered: %v. %s", r, string(debug.Stack()))
-	if err := files.AppendToFile(p.filePath, []byte(tracesToWrite)); err != nil {
-		return fmt.Errorf("WriteStackTracesToFile: failed to write to file %s: %w", p.filePath, err)
+	if err := appendToFile(p.filePath, []byte(tracesToWrite)); err != nil {
+		return fmt.Errorf("WriteStackTracesToFile: failed to write to %s: %w", p.filePath, err)
 	}
 	return nil
+}
+
+// appendToFile appends data to a file. If the file does not exist, it will be created.
+func appendToFile(filename string, data []byte) (err error) {
+	defer func() {
+		if err != nil {
+			err = fmt.Errorf("appendToFile: %w", err)
+		}
+	}()
+
+	// Open file with append mode (O_APPEND), create if it doesn't exist (O_CREATE),
+	// and set write permission (O_WRONLY)
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	// Write the data to the file
+	_, err = file.Write(data)
+	if err != nil {
+		_ = file.Close()
+		return err
+	}
+	_ = file.Close()
+	return err
 }
