385e576db5eb2b11000d5cba7ba41851adfe6308
Brian Goad <bdgoad@gmail.com>
2025-01-31 21:06:33 -0500

    Journey test fix again

--- genesyscloud/util/testrunner/testrunner.go
+++ genesyscloud/util/testrunner/testrunner.go
@@ -78,10 +78,7 @@ func GenerateResourceJourneyTestSteps(resourceType string, testCaseName string, 
 func GenerateJourneyTestSteps(testType string, resourceType string, testCaseName string, checkFuncs []resource.TestCheckFunc) []resource.TestStep {
 	var testSteps []resource.TestStep
 	var testCasePath string
-	testCasePath = GetTestDataPath(testType, resourceType)
-	if resourceType == "genesyscloud_journey_action_map" || resourceType == "genesyscloud_journey_action_template" || resourceType == "genesyscloud_journey_outcome" {
-		testCasePath = filepath.Join(testCasePath, testCaseName)
-	}
+	testCasePath = GetTestDataPath(testType, resourceType, testCaseName)
 	testCaseDirEntries, _ := os.ReadDir(testCasePath)
 	checkFuncIndex := 0
 	for _, testCaseDirEntry := range testCaseDirEntries {
