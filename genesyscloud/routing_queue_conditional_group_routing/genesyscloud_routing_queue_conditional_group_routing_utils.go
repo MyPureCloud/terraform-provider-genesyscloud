bfbaef4dd98f3d9bdad899ee7c700fcd324cc10d
Charlie Conneely <charlie.conneely@genesys.com>
2025-02-05 16:09:15 +0000

    Converting groups in CGR resource to TypeSet

---
+++ genesyscloud/routing_queue_conditional_group_routing/genesyscloud_routing_queue_conditional_group_routing_utils.go
@@ -0,0 +1,67 @@
+package routing_queue_conditional_group_routing
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/mypurecloud/platform-client-sdk-go/v150/platformclientv2"
+	routingQueue "terraform-provider-genesyscloud/genesyscloud/routing_queue"
+	"terraform-provider-genesyscloud/genesyscloud/util/resourcedata"
+)
+
+func buildConditionalGroupRouting(rules []interface{}) ([]platformclientv2.Conditionalgrouproutingrule, error) {
+	var sdkRules []platformclientv2.Conditionalgrouproutingrule
+	for i, rule := range rules {
+		configRule := rule.(map[string]interface{})
+		sdkRule := platformclientv2.Conditionalgrouproutingrule{
+			Operator:       platformclientv2.String(configRule["operator"].(string)),
+			ConditionValue: platformclientv2.Float64(configRule["condition_value"].(float64)),
+		}
+
+		if evaluatedQueue, ok := configRule["evaluated_queue_id"].(string); ok && evaluatedQueue != "" {
+			if i == 0 {
+				return nil, fmt.Errorf("for rule 1, the current queue is used so evaluated_queue_id should not be specified")
+			}
+			sdkRule.Queue = &platformclientv2.Domainentityref{Id: &evaluatedQueue}
+		}
+
+		resourcedata.BuildSDKStringValueIfNotNil(&sdkRule.Metric, configRule, "metric")
+		if waitSeconds, ok := configRule["wait_seconds"].(int); ok {
+			sdkRule.WaitSeconds = &waitSeconds
+		}
+
+		if memberGroupSet, ok := configRule["groups"].(*schema.Set); ok {
+			sdkRule.Groups = routingQueue.BuildCGRGroups(memberGroupSet)
+		}
+
+		sdkRules = append(sdkRules, sdkRule)
+	}
+
+	return sdkRules, nil
+}
+
+func flattenConditionalGroupRouting(sdkRules *[]platformclientv2.Conditionalgrouproutingrule) []interface{} {
+	if sdkRules == nil {
+		return nil
+	}
+
+	var rules []interface{}
+	for i, sdkRule := range *sdkRules {
+		rule := make(map[string]interface{})
+
+		// The first rule is assumed to apply to this queue, so evaluated_queue_id should be omitted
+		if i > 0 {
+			resourcedata.SetMapReferenceValueIfNotNil(rule, "evaluated_queue_id", sdkRule.Queue)
+		}
+		resourcedata.SetMapValueIfNotNil(rule, "wait_seconds", sdkRule.WaitSeconds)
+		resourcedata.SetMapValueIfNotNil(rule, "operator", sdkRule.Operator)
+		resourcedata.SetMapValueIfNotNil(rule, "condition_value", sdkRule.ConditionValue)
+		resourcedata.SetMapValueIfNotNil(rule, "metric", sdkRule.Metric)
+
+		if sdkRule.Groups != nil {
+			rule["groups"] = routingQueue.FlattenCGRGroups(*sdkRule.Groups)
+		}
+
+		rules = append(rules, rule)
+	}
+	return rules
+}
