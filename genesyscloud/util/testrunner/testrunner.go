e54b1a9b2182a99b60a85f7fe0f5c4390b1d93b8
HemanthDogiparthi12 <hemanth.dogiparthi@genesys.com>
2025-02-10 18:54:58 +0000

    fix platform init problem

--- genesyscloud/util/testrunner/testrunner.go
+++ genesyscloud/util/testrunner/testrunner.go
@@ -24,10 +24,6 @@ const (
 
 var RootDir string
 
-func init() {
-	RootDir = getRootDir()
-}
-
 // Helper function that retrieves the location of the root directory
 func getRootDir() string {
 	_, filename, _, ok := runtime.Caller(0)
@@ -56,13 +52,13 @@ func getRootDir() string {
 }
 
 func GetTestDataPath(elem ...string) string {
-	basePath := filepath.Join(RootDir, "test", "data")
+	basePath := filepath.Join(getRootDir(), "test", "data")
 	subPath := filepath.Join(elem...)
 	return filepath.Join(basePath, subPath)
 }
 
 func GetTestTempPath(elem ...string) string {
-	basePath := filepath.Join(RootDir, "test", "temp")
+	basePath := filepath.Join(getRootDir(), "test", "temp")
 	subPath := filepath.Join(elem...)
 	return filepath.Join(basePath, subPath)
 }
