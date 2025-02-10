08e0c1262cc28af608ce66ef7cd32414bc1a1b8d
HemanthDogiparthi12 <hemanth.dogiparthi@genesys.com>
2025-02-10 20:25:38 +0000

    fix platform dependency for regression

--- genesyscloud/tfexporter/tfstate_exporter.go
+++ genesyscloud/tfexporter/tfstate_exporter.go
@@ -5,6 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 	"terraform-provider-genesyscloud/genesyscloud/platform"
 	resourceExporter "terraform-provider-genesyscloud/genesyscloud/resource_exporter"
@@ -47,12 +50,6 @@ func NewTFStateWriter(ctx context.Context, resources []resourceExporter.Resource
 }
 
 func (t *TFStateFileWriter) writeTfState() diag.Diagnostics {
-
-	platform := platform.GetPlatform()
-	platformErr := platform.Validate()
-	if platformErr != nil {
-		return diag.Errorf("Failed to validate platform: %v", platformErr)
-	}
 
 	stateFilePath, diagErr := getFilePath(t.d, defaultTfStateFile)
 	if diagErr != nil {
@@ -101,6 +98,16 @@ func (t *TFStateFileWriter) writeTfState() diag.Diagnostics {
 		return diagErr
 	}
 
+	platform := platform.GetPlatform()
+	platformErr := platform.Validate()
+	if platformErr != nil {
+		log.Printf("Failed to validate platform: %v", platformErr)
+		err := t.writeTfStateLegacy(stateFilePath)
+		if err != nil {
+			return nil
+		}
+	}
+
 	// This outputs terraform state v3, and there is currently no public lib to generate v4 which is required for terraform 0.13+.
 	// However, the state can be upgraded automatically by calling the terraform CLI. If this fails, just print a warning indicating
 	// that the state likely needs to be upgraded manually.
@@ -132,6 +139,57 @@ func (t *TFStateFileWriter) writeTfState() diag.Diagnostics {
 			%s`, err, cliErrorPostscript)
 		log.Print(cliErrorPostscript)
 		// Don't fail everything even if this errors.
+		err := t.writeTfStateLegacy(stateFilePath)
+		if err != nil {
+			return nil
+		}
+		return nil
+	}
+	return nil
+}
+
+func (t *TFStateFileWriter) writeTfStateLegacy(stateFilePath string) error {
+	cliError := `Failed to run the terraform CLI to upgrade the generated state file.
+	The generated tfstate file will need to be upgraded manually by running the
+	following in the state file's directory:
+	'terraform state replace-provider registry.terraform.io/-/genesyscloud registry.terraform.io/mypurecloud/genesyscloud'`
+
+	tfpath, err := exec.LookPath("terraform")
+	if err != nil {
+		log.Println("Failed to find terraform path:", err)
+		log.Println(cliError)
+		return nil
+	}
+
+	// exec.CommandContext does not auto-resolve symlinks
+	fileInfo, err := os.Lstat(tfpath)
+	if err != nil {
+		log.Println("Failed to Lstat terraform path:", err)
+		log.Println(cliError)
+		return nil
+	}
+	if fileInfo.Mode()&os.ModeSymlink != 0 {
+		tfpath, err = filepath.EvalSymlinks(tfpath)
+		if err != nil {
+			log.Println("Failed to resolve terraform path symlink:", err)
+			log.Println(cliError)
+			return nil
+		}
+	}
+
+	cmd := exec.CommandContext(t.ctx, tfpath)
+	cmd.Args = append(cmd.Args, []string{
+		"state",
+		"replace-provider",
+		"-auto-approve",
+		"-state=" + stateFilePath,
+		"registry.terraform.io/-/genesyscloud",
+		t.providerRegistry,
+	}...)
+	log.Printf("Running 'terraform state replace-provider' on %s", stateFilePath)
+	if err = cmd.Run(); err != nil {
+		log.Println("Failed to run command:", err)
+		log.Println(cliError)
 		return nil
 	}
 	return nil
