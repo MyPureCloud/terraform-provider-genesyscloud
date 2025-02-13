e0fcbbb9b8b5d4fabc05dea37ae4ab931f037b06
Charlie Conneely <charlie.conneely@genesys.com>
2025-02-13 11:43:13 +0000

    Removed duplicate function and small refactor

--- genesyscloud/user/genesyscloud_user_proxy.go
+++ genesyscloud/user/genesyscloud_user_proxy.go
@@ -310,22 +310,7 @@ func getVoicemailUserpoliciesByUserIdFn(ctx context.Context, p *userProxy, id st
 
 func updatePasswordFn(ctx context.Context, p *userProxy, userId string, newPassword string) (*platformclientv2.APIResponse, error) {
 	// Get the user's current password
-	resp, err := p.userApi.PostUserPassword(userId, platformclientv2.Changepasswordrequest{
+	return p.userApi.PostUserPassword(userId, platformclientv2.Changepasswordrequest{
 		NewPassword: &newPassword,
 	})
-	if err != nil {
-		return resp, err
-	}
-	return resp, nil
-}
-
-func updatePasswordFn(ctx context.Context, p *userProxy, userId string, newPassword string) (*platformclientv2.APIResponse, error) {
-	// Get the user's current password
-	resp, err := p.userApi.PostUserPassword(userId, platformclientv2.Changepasswordrequest{
-		NewPassword: &newPassword,
-	})
-	if err != nil {
-		return resp, err
-	}
-	return resp, nil
 }
