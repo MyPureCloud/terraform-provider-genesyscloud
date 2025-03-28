a9c7000e4f3ab61f4e5c6eaa82604f1c9c3d6cb7
Matthew Hallinan <matthew.hallinan@genesys.com>
2025-03-28 16:50:06 +0000

    GPE-14858 Add deprecation for Scope

--- genesyscloud/journey_segment/resource_genesyscloud_journey_segment_schema.go
+++ genesyscloud/journey_segment/resource_genesyscloud_journey_segment_schema.go
@@ -52,6 +52,7 @@ var (
 			Required:     true,
 			ForceNew:     true, // scope can be only set during creation
 			ValidateFunc: validation.StringInSlice([]string{"Session"}, false),
+			Deprecated:   "The scope field is redundant and will be removed in a future release.",
 		},
 		"should_display_to_agent": {
 			Description: "Whether or not the segment should be displayed to agent/supervisor users.",
