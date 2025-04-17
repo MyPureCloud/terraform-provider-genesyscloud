64c71514e83166d66d230e112d5ce249a6d52823
Brian Goad <bdgoad@gmail.com>
2025-04-17 04:53:16 -0400

    Feature DEVTOOLING-1182 Adds a new sanitizer that is dedicated to the needs of BCP (#1665)
    * Adds a new sanitizer that is dedicated to the needs of BCP. Also adds some extra context and comments, as well as tests for resource_name_sanitizer
    
    * Docs updates, mark tests as unit tests

--- genesyscloud/util/feature_toggles/exporter_sanitizer.go
+++ genesyscloud/util/feature_toggles/exporter_sanitizer.go
@@ -3,13 +3,33 @@ package feature_toggles
 import "os"
 
 const sanitizerOptimized = "GENESYS_SANITIZER_OPTIMIZED"
+const enableOptimizedSanitizer = "ENABLE_OPTIMIZED_SANITIZER"
 
 func ExporterSanitizerOptimizedName() string {
-	return sanitizerOptimized
+	return enableOptimizedSanitizer
 }
 
 func ExporterSanitizerOptimizedToggleExists() bool {
 	var exists bool
-	_, exists = os.LookupEnv(sanitizerOptimized)
+	_, sanitizerOptimizedExists := os.LookupEnv(sanitizerOptimized)
+	_, enableOptimizedSanitizer := os.LookupEnv(enableOptimizedSanitizer)
+	if sanitizerOptimizedExists || enableOptimizedSanitizer {
+		exists = true
+	}
+	return exists
+}
+
+const enableBCPOptimizedSanitizer = "ENABLE_BCP_OPTIMIZED_SANITIZER"
+
+func ExporterSanitizerBCPOptimizedName() string {
+	return enableBCPOptimizedSanitizer
+}
+
+func ExporterSanitizerBCPOptimizedToggleExists() bool {
+	var exists bool
+	_, enableBCPOptimizedSanitizer := os.LookupEnv(enableBCPOptimizedSanitizer)
+	if enableBCPOptimizedSanitizer {
+		exists = true
+	}
 	return exists
 }
