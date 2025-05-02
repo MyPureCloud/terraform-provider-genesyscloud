5a3f8191a28ea61a575c17beda4e840361b2c86c
Brian Goad <bdgoad@gmail.com>
2025-05-02 15:15:11 -0400

    More examples supported

---
+++ examples/resources/genesyscloud_orgauthorization_pairing/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  dependencies = [
+    "../genesyscloud_group/resource.tf",
+    "../genesyscloud_user/resource.tf",
+  ]
+}
--- examples/resources/genesyscloud_orgauthorization_pairing/resource.tf
+++ examples/resources/genesyscloud_orgauthorization_pairing/resource.tf
@@ -1,4 +1,4 @@
 resource "genesyscloud_orgauthorization_pairing" "example_orgauthorization_pairing" {
-  user_ids  = [genesyscloud_user.user-1.id, genesyscloud_user.user-2.id]
-  group_ids = [genesyscloud_group.group.id]
+  user_ids  = [genesyscloud_user.example_user.id, genesyscloud_user.example_user2.id]
+  group_ids = [genesyscloud_group.example_group.id]
 }
