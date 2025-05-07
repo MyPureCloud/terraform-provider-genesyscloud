c0cfba362d1cd7833f9eea3d7dabb62eccac06d6
Brian Goad <bdgoad@gmail.com>
2025-05-07 14:36:49 -0400

    NO-JIRA - Fixes error handling in genesys_recording_media_retention_policy resource in order to surface specific errors to the user

--- genesyscloud/recording_media_retention_policy/resource_genesyscloud_recording_media_retention_policy.go
+++ genesyscloud/recording_media_retention_policy/resource_genesyscloud_recording_media_retention_policy.go
@@ -67,16 +67,16 @@ func createMediaRetentionPolicy(ctx context.Context, d *schema.ResourceData, met
 	order := d.Get("order").(int)
 	description := d.Get("description").(string)
 	enabled := d.Get("enabled").(bool)
-	err, mediaPolicies := buildMediaPolicies(d, pp, ctx)
+	mediaPolicies, err := buildMediaPolicies(d, pp, ctx)
 
 	if err != nil {
-		util.BuildDiagnosticError(ResourceType, "error while calling buildMediaPolicie()in createMediaRetention", err)
+		return util.BuildDiagnosticError(ResourceType, "error while calling buildMediaPolicies()in createMediaRetention", err)
 	}
 
 	conditions := buildConditions(d)
-	err, actions := buildPolicyActionsFromResource(d, pp, ctx)
+	actions, err := buildPolicyActionsFromResource(d, pp, ctx)
 	if err != nil {
-		util.BuildDiagnosticError(ResourceType, "error while calling buildPolicyActionsFromResource()", err)
+		return util.BuildDiagnosticError(ResourceType, "error while calling buildPolicyActionsFromResource()", err)
 	}
 
 	policyErrors := buildPolicyErrors(d)
@@ -132,7 +132,7 @@ func readMediaRetentionPolicy(ctx context.Context, d *schema.ResourceData, meta 
 
 		err, mediaPolicies := flattenMediaPolicies(retentionPolicy.MediaPolicies, pp, ctx)
 		if err != nil {
-			return retry.NonRetryableError(fmt.Errorf("Unable to flatten media policies in readMediaRetentionPolicy() method: %s", err))
+			return retry.NonRetryableError(fmt.Errorf("unable to flatten media policies in readMediaRetentionPolicy() method: %s", err))
 		}
 		if retentionPolicy.MediaPolicies != nil {
 			d.Set("media_policies", mediaPolicies)
@@ -140,7 +140,7 @@ func readMediaRetentionPolicy(ctx context.Context, d *schema.ResourceData, meta 
 
 		err, actions := flattenPolicyActions(retentionPolicy.Actions, pp, ctx)
 		if err != nil {
-			return retry.NonRetryableError(fmt.Errorf("Unable to flatten actions in readMediaRetentionPolicy(): %s", err))
+			return retry.NonRetryableError(fmt.Errorf("unable to flatten actions in readMediaRetentionPolicy(): %s", err))
 		}
 		if retentionPolicy.Actions != nil {
 			d.Set("actions", actions)
@@ -158,13 +158,13 @@ func updateMediaRetentionPolicy(ctx context.Context, d *schema.ResourceData, met
 	order := d.Get("order").(int)
 	description := d.Get("description").(string)
 	enabled := d.Get("enabled").(bool)
-	err, mediaPolicies := buildMediaPolicies(d, pp, ctx)
+	mediaPolicies, err := buildMediaPolicies(d, pp, ctx)
 	if err != nil {
 		return util.BuildDiagnosticError(ResourceType, "Error while retrieving buildMediaPolicies() function in updateMediaRetentionPolicy() method)", err)
 	}
 
 	conditions := buildConditions(d)
-	err, actions := buildPolicyActionsFromResource(d, pp, ctx)
+	actions, err := buildPolicyActionsFromResource(d, pp, ctx)
 
 	if err != nil {
 		return util.BuildDiagnosticError(ResourceType, "Error while retrieving buildPolicyActionsFromResource() function in updateMediaRetentionPolicy() method)", err)
