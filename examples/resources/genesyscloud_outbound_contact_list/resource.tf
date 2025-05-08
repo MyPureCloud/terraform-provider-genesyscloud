d9b4cc1ce8e1dcd06da91d2aa257f80698a7034c
Brian Goad <bdgoad@gmail.com>
2025-05-08 12:11:26 -0400

    Bump platform

--- examples/resources/genesyscloud_outbound_contact_list/resource.tf
+++ examples/resources/genesyscloud_outbound_contact_list/resource.tf
@@ -2,6 +2,7 @@ resource "genesyscloud_outbound_contact_list" "contact_list" {
   name             = "Example Contact List"
   column_names     = ["First Name", "Last Name", "Cell", "Home", "Email", "Zipcode", "Timezone"]
   attempt_limit_id = genesyscloud_outbound_attempt_limit.attempt_limit.id
+
   phone_columns {
     column_name          = "Cell"
     type                 = "cell"
