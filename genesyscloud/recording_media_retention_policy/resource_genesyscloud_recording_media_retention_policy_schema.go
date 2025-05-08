c46e55539577538c9b86f152483988bc726c87b0
Brian Goad <bdgoad@gmail.com>
2025-05-08 10:27:59 -0400

    NO-JIRA - Fixes error handling in genesys_recording_media_retention_policy resource in order to surface specific errors to the user (#1699)

--- genesyscloud/recording_media_retention_policy/resource_genesyscloud_recording_media_retention_policy_schema.go
+++ genesyscloud/recording_media_retention_policy/resource_genesyscloud_recording_media_retention_policy_schema.go
@@ -477,9 +477,14 @@ func ResourceMediaRetentionPolicy() *schema.Resource {
 		},
 	}
 
-	agentTimeInterval := &schema.Resource{
+	evalTimeInterval := &schema.Resource{
 		Schema: map[string]*schema.Schema{
-			"months": {
+			"hours": {
+				Description: "",
+				Type:        schema.TypeInt,
+				Optional:    true,
+			},
+			"days": {
 				Description: "",
 				Type:        schema.TypeInt,
 				Optional:    true,
@@ -489,22 +494,7 @@ func ResourceMediaRetentionPolicy() *schema.Resource {
 				Type:        schema.TypeInt,
 				Optional:    true,
 			},
-			"days": {
-				Description: "",
-				Type:        schema.TypeInt,
-				Optional:    true,
-			},
-		},
-	}
-
-	evalTimeInterval := &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			"hours": {
-				Description: "",
-				Type:        schema.TypeInt,
-				Optional:    true,
-			},
-			"days": {
+			"months": {
 				Description: "",
 				Type:        schema.TypeInt,
 				Optional:    true,
@@ -594,7 +584,7 @@ func ResourceMediaRetentionPolicy() *schema.Resource {
 				Type:        schema.TypeList,
 				MaxItems:    1,
 				Optional:    true,
-				Elem:        agentTimeInterval,
+				Elem:        evalTimeInterval,
 			},
 			"time_zone": {
 				Description: "",
