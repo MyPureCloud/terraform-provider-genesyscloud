4791332d7545d68b5d696b050cbc7eab38addec2
Brian Goad <bdgoad@gmail.com>
2025-05-16 10:19:25 -0400

    NO-JIRA fix issue with flow outcomes where a creation fails if it already exists. (#1702)
    * NO-JIRA fix issue with flow outcomes where a creation fails if it already exists. Because these resources cannot be deleted, we should be able to attach to the existing one and update it
    
    * As per PR review
    
    * Quality of life updates as per IDE linter

--- genesyscloud/flow_outcome/resource_genesyscloud_flow_outcome.go
+++ genesyscloud/flow_outcome/resource_genesyscloud_flow_outcome.go
@@ -3,11 +3,12 @@ package flow_outcome
 import (
 	"context"
 	"fmt"
+	"log"
+
 	"github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/provider"
 	resourceExporter "github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/resource_exporter"
 	"github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/util"
 	"github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/util/constants"
-	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -44,6 +45,19 @@ func getAllAuthFlowOutcomes(ctx context.Context, clientConfig *platformclientv2.
 func createFlowOutcome(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	sdkConfig := meta.(*provider.ProviderMeta).ClientConfig
 	proxy := getFlowOutcomeProxy(sdkConfig)
+
+	flowOutcomeName := d.Get("name").(string)
+
+	// Since Flow Outcomes cannot be deleted, we need to be able to check if there is an existing Flow Outcome and update it
+	flowOutcomeId, retryable, _, err := proxy.getFlowOutcomeIdByName(ctx, flowOutcomeName)
+	if flowOutcomeId != "" {
+		d.SetId(flowOutcomeId)
+		return updateFlowOutcome(ctx, d, meta)
+	}
+
+	if err != nil && !retryable {
+		return util.BuildAPIDiagnosticError(ResourceType, fmt.Sprintf("Failed to check for existing flow outcomes when creating flow outcome %s error: %s", flowOutcomeName, err), nil)
+	}
 
 	flowOutcome := getFlowOutcomeFromResourceData(d)
 
