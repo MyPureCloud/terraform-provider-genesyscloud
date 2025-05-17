f3601d9bc48b4a6faf698ef432bcf84fa2d09a10
Brian Goad <bdgoad@gmail.com>
2025-05-17 14:06:25 -0400

    NO-JIRA - A few other small fixes found in various resource types while working on DEVTOOLING-1189

--- genesyscloud/routing_queue/resource_genesyscloud_routing_queue_schema.go
+++ genesyscloud/routing_queue/resource_genesyscloud_routing_queue_schema.go
@@ -12,7 +12,6 @@ import (
 	responseManagementLibrary "github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/responsemanagement_library"
 	routingSkill "github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/routing_skill"
 	routingSkillGroup "github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/routing_skill_group"
-	routingSmsAddresses "github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/routing_sms_addresses"
 	routingWrapupcode "github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/routing_wrapupcode"
 	"github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/scripts"
 	"github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/team"
@@ -658,7 +657,6 @@ func RoutingQueueExporter() *resourceExporter.ResourceExporter {
 			"message_in_queue_flow_id":                          {RefType: architectFlow.ResourceType},
 			"whisper_prompt_id":                                 {RefType: architectUserPrompt.ResourceType},
 			"on_hold_prompt_id":                                 {RefType: architectUserPrompt.ResourceType},
-			"outbound_messaging_sms_address_id":                 {RefType: routingSmsAddresses.ResourceType}, // Ref type not yet defined
 			"default_script_ids.*":                              {RefType: scripts.ResourceType},
 			"outbound_email_address.route_id":                   {RefType: "genesyscloud_routing_email_route"},  // must be hard-coded to avoid import cycle
 			"outbound_email_address.domain_id":                  {RefType: "genesyscloud_routing_email_domain"}, // must be hard-coded to avoid import cycle
