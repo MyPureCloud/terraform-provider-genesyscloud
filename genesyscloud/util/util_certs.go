07118d7e689370edbaa21ff852544650465ea0cc
Brian Goad <bdgoad@gmail.com>
2025-11-20 12:26:22 -0500

    Fix SuppressCertDiff function

--- genesyscloud/util/util_certs.go
+++ genesyscloud/util/util_certs.go
@@ -1,12 +1,20 @@
 package util
 
 import (
+	"log"
 	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func SuppressCertificateDiff(k, old, new string, d *schema.ResourceData) bool {
+	lastDotIndex := strings.LastIndex(k, ".")
+	if lastDotIndex != -1 {
+		k = string(k[:lastDotIndex])
+	}
+	o, n := d.GetChange(k)
+	log.Printf("%s, %s", o, n)
+
 	return normalizeCertificate(old) == normalizeCertificate(new)
 }
 
