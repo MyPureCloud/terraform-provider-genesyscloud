0bf998418634c70e004fe1ff5ea638a02b75f30a
mcphee11 <66649147+mcphee11@users.noreply.github.com>
2025-11-27 23:29:34 +1100

    Feature/ai studio summary (#1983)
    * summary inital commit
    
    * added pii & lables
    
    * added pii & lables to Read
    
    * added tests
    
    * added resource parms
    
    * updates per PR1983, add cache and update getAllFunc with name
    
    * updates per PR1983, rename from aistudio_summary_settings to ai_studio_summary_settings including varible naming in files
    
    * updates per PR1983, add documentation and updated api links for generation of docs
    
    * updates per PR1983, consolidate getAllAiStudioSummarySettingFn and add cache to func
    
    * updates per PR1983, add ValidateFunc where possible
    
    * updates per PR1983, remove external_system_url from tests and fix naming typo in generateAiSummarySettingDataSource test
    
    * updates per PR1983, remove onto new line for test formatting
    
    * updates per PR1983, fix predefined_insights = [ '%s', '%s' ] in test
    
    * updates per PR1983, predefined_insights change in schema as well as tests... not 100% on this one...
    
    * updates per PR1983, removed loops in test and added len() check as arry len fixed
    
    * updates per PR1983, clean up the 2x flattern func to remove loops
    
    * updates per PR1983, remove additional items from test
    
    * updates per PR1983, make items resource.GetNonZeroPointer that are not required
    
    * updates per PR1983, fixed items in tests after schema updates
    
    * updates per PR1983, remove unnecessary nil check on items
    
    * updates per PR1983, set to SetToStringList to support TypeSet
    
    * updates per PR1983, update name and resourceName items

---
+++ examples/data-sources/genesyscloud_ai_studio_summary_setting/data-source.tf
@@ -0,0 +1,3 @@
+data "genesyscloud_ai_studio_summary_setting" "summary_setting" {
+  name = "my_summary_setting"
+}
---
+++ genesyscloud/ai_studio_summary_setting/resource_genesyscloud_ai_studio_summary_setting_test.go
@@ -0,0 +1,99 @@
+package ai_studio_summary_setting
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/provider"
+	"github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/util"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+/*
+The resource_genesyscloud_ai_studio_summary_setting_test.go contains all of the test cases for running the resource
+tests for ai_studio_summary_setting.
+*/
+
+func TestAccResourceAiStudioSummarySetting(t *testing.T) {
+	//t.Parallel()
+	var (
+		resourceName             = "test-summary-setting"
+		name                     = "summary setting test"
+		language                 = "en-au"
+		summaryType              = "Concise"
+		settingType              = "Basic"
+		format                   = "BulletPoints"
+		maskPii                  = "true"
+		participantLabelInternal = "Advisor"
+		participantLabelExternal = "Member"
+		predefinedInsights0      = "ReasonForCall"
+		predefinedInsights1      = "Resolution"
+		prompt                   = "Summaries should be no more then 300 characters and include 3 dot points of key information"
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { util.TestAccPreCheck(t) },
+		ProviderFactories: provider.GetProviderFactories(providerResources, providerDataSources),
+		Steps: []resource.TestStep{
+			{
+				// Create
+				Config: GenerateBasicSummarySettingResource(resourceName, name, language, ""),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("genesyscloud_ai_studio_summary_setting."+resourceName, "name", name),
+					resource.TestCheckResourceAttr("genesyscloud_ai_studio_summary_setting."+resourceName, "language", language),
+				),
+			},
+			{
+				// Update
+				Config: GenerateFullAiStudioSummarySettingResource(resourceName, name, language, summaryType, settingType, format, maskPii, participantLabelInternal, participantLabelExternal, predefinedInsights0, predefinedInsights1, prompt),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("genesyscloud_ai_studio_summary_setting."+resourceName, "name", name),
+					resource.TestCheckResourceAttr("genesyscloud_ai_studio_summary_setting."+resourceName, "language", language),
+					resource.TestCheckResourceAttr("genesyscloud_ai_studio_summary_setting."+resourceName, "summary_type", summaryType),
+					resource.TestCheckResourceAttr("genesyscloud_ai_studio_summary_setting."+resourceName, "setting_type", settingType),
+					resource.TestCheckResourceAttr("genesyscloud_ai_studio_summary_setting."+resourceName, "format", format),
+					resource.TestCheckResourceAttr("genesyscloud_ai_studio_summary_setting."+resourceName, "mask_p_i_i.0.all", maskPii),
+					resource.TestCheckResourceAttr("genesyscloud_ai_studio_summary_setting."+resourceName, "participant_labels.0.internal", participantLabelInternal),
+					resource.TestCheckResourceAttr("genesyscloud_ai_studio_summary_setting."+resourceName, "participant_labels.0.external", participantLabelExternal),
+					resource.TestCheckResourceAttr("genesyscloud_ai_studio_summary_setting."+resourceName, "predefined_insights.0", predefinedInsights0),
+					resource.TestCheckResourceAttr("genesyscloud_ai_studio_summary_setting."+resourceName, "predefined_insights.1", predefinedInsights1),
+					resource.TestCheckResourceAttr("genesyscloud_ai_studio_summary_setting."+resourceName, "prompt", prompt),
+				),
+			},
+			{
+				// Import/Read
+				ResourceName:      "genesyscloud_ai_studio_summary_setting." + resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+		CheckDestroy: testVerifyAiStudioSummarySettingDestroyed,
+	})
+}
+
+func GenerateFullAiStudioSummarySettingResource(resourceName, name, language, summaryType, settingType, format, maskPii, participantLabelInternal, participantLabelExternal, predefinedInsights0, predefinedInsights1, prompt string) string {
+	return fmt.Sprintf(`resource "genesyscloud_ai_studio_summary_setting" "%s" {
+		name             = "%s"
+		language         = "%s"
+		summary_type     = "%s"
+		setting_type     = "%s"
+		format           = "%s"
+		mask_p_i_i {
+		all = %s
+		}
+		participant_labels {
+			internal = "%s"
+			external = "%s"
+		}
+		predefined_insights = [ "%s", "%s" ]
+		prompt = "%s"
+	}
+	`, resourceName, name, language, summaryType, settingType, format, maskPii, participantLabelInternal, participantLabelExternal, predefinedInsights0, predefinedInsights1, prompt)
+}
+
+func testVerifyAiStudioSummarySettingDestroyed(state *terraform.State) error {
+	return nil
+}
