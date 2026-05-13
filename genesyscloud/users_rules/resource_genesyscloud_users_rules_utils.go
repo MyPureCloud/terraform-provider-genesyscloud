a677840f6a5c98b239a4273ef6536700d3fa88fd
reginald-cui-genesys <77636863+reginald-cui-genesys@users.noreply.github.com>
2026-05-13 08:04:04 -0700

    PDNA-26733: add learning modules and users rules resources (#2098)
    * PDNA-26733: add learning modules resource
    
    * PDNA-26733: finish user rules
    
    * PDNA-26733: add new resources to registrar
    
    * PDNA-26733: add users rules API links
    
    * PDNA-26733: add autoAssign
    
    * PDNA-26733: update types
    
    * PDNA-26733: improve logging
    
    * PDNA-26733: fix error handling and tests
    
    * PDNA-26733: update schemas and code to allow empty description and fetching of user rules
    
    * PDNA-26733: update to continue
    
    * PDNA-26733: remove unneeded import block and variables
    
    * PDNA-26733: generate docs

---
+++ genesyscloud/users_rules/resource_genesyscloud_users_rules_utils.go
@@ -0,0 +1,240 @@
+package users_rules
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/mypurecloud/platform-client-sdk-go/v179/platformclientv2"
+	"github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/util/resourcedata"
+)
+
+type UsersRulesValueStruct struct {
+	ContextId string
+	Ids       []string
+}
+
+type UsersRulesGroupItemStruct struct {
+	Id        string
+	Operator  string
+	Container string
+	Values    []UsersRulesValueStruct
+}
+
+type UsersRulesCriteriaStruct struct {
+	Id       string
+	Operator string
+	Group    []UsersRulesGroupItemStruct
+}
+
+func buildSdkUsersRulesValue(resourceUsersRulesValueList []interface{}) *[]platformclientv2.Usersrulesvalue {
+	usersRulesValues := make([]platformclientv2.Usersrulesvalue, 0)
+
+	for _, usersRulesValue := range resourceUsersRulesValueList {
+		usersRulesValueMap := usersRulesValue.(map[string]interface{})
+
+		var sdkUsersRulesValue platformclientv2.Usersrulesvalue
+		resourcedata.BuildSDKStringValueIfNotNil(&sdkUsersRulesValue.ContextId, usersRulesValueMap, "context_id")
+		resourcedata.BuildSDKStringArrayValueIfNotNil(&sdkUsersRulesValue.Ids, usersRulesValueMap, "ids")
+
+		usersRulesValues = append(usersRulesValues, sdkUsersRulesValue)
+	}
+
+	return &usersRulesValues
+}
+
+func buildSdkUsersRulesGroupItem(resourceUsersRulesGroupItemList []interface{}) *[]platformclientv2.Usersrulesgroupitem {
+	usersRulesGroupItems := make([]platformclientv2.Usersrulesgroupitem, 0)
+
+	for _, usersRulesGroupItem := range resourceUsersRulesGroupItemList {
+		usersRulesGroupItemMap := usersRulesGroupItem.(map[string]interface{})
+
+		var sdkUsersRulesGroupItem platformclientv2.Usersrulesgroupitem
+		resourcedata.BuildSDKStringValueIfNotNil(&sdkUsersRulesGroupItem.Id, usersRulesGroupItemMap, "id")
+		sdkUsersRulesGroupItem.Operator = platformclientv2.String(usersRulesGroupItemMap["operator"].(string))
+		sdkUsersRulesGroupItem.Container = platformclientv2.String(usersRulesGroupItemMap["container"].(string))
+		sdkUsersRulesGroupItem.Values = buildSdkUsersRulesValue(usersRulesGroupItemMap["values"].([]interface{}))
+
+		usersRulesGroupItems = append(usersRulesGroupItems, sdkUsersRulesGroupItem)
+	}
+
+	return &usersRulesGroupItems
+}
+
+func buildSdkUsersRulesCriteria(resourceUsersRulesCriteriaList []interface{}) *[]platformclientv2.Usersrulescriteria {
+	if resourceUsersRulesCriteriaList == nil {
+		return nil
+	}
+
+	usersRulesCriteriaList := make([]platformclientv2.Usersrulescriteria, 0)
+
+	for _, usersRulesCriteria := range resourceUsersRulesCriteriaList {
+		usersRulesCriteriaMap := usersRulesCriteria.(map[string]interface{})
+
+		var sdkUsersRulesCriteria platformclientv2.Usersrulescriteria
+		resourcedata.BuildSDKStringValueIfNotNil(&sdkUsersRulesCriteria.Id, usersRulesCriteriaMap, "id")
+		sdkUsersRulesCriteria.Operator = platformclientv2.String(usersRulesCriteriaMap["operator"].(string))
+		sdkUsersRulesCriteria.Group = buildSdkUsersRulesGroupItem(usersRulesCriteriaMap["group"].([]interface{}))
+
+		usersRulesCriteriaList = append(usersRulesCriteriaList, sdkUsersRulesCriteria)
+	}
+
+	return &usersRulesCriteriaList
+}
+
+func flattenUsersRulesValue(usersRulesValues *[]platformclientv2.Usersrulesvalue) []interface{} {
+	if usersRulesValues == nil {
+		return nil
+	}
+
+	usersRulesValuesList := make([]interface{}, 0)
+	for _, usersRulesValue := range *usersRulesValues {
+		usersRulesValueMap := make(map[string]interface{})
+
+		if usersRulesValue.ContextId != nil {
+			usersRulesValueMap["context_id"] = *usersRulesValue.ContextId
+		}
+		if usersRulesValue.Ids != nil {
+			usersRulesValueMap["ids"] = *usersRulesValue.Ids
+		}
+
+		usersRulesValuesList = append(usersRulesValuesList, usersRulesValueMap)
+	}
+
+	return usersRulesValuesList
+}
+
+func flattenUsersRulesGroupItem(usersRulesGroupItem *[]platformclientv2.Usersrulesgroupitem) []interface{} {
+	if usersRulesGroupItem == nil {
+		return nil
+	}
+
+	usersRulesGroupItemList := make([]interface{}, 0)
+	for _, usersRulesGroupItem := range *usersRulesGroupItem {
+		usersRulesGroupItemMap := make(map[string]interface{})
+
+		if usersRulesGroupItem.Id != nil {
+			usersRulesGroupItemMap["id"] = *usersRulesGroupItem.Id
+		}
+		if usersRulesGroupItem.Operator != nil {
+			usersRulesGroupItemMap["operator"] = *usersRulesGroupItem.Operator
+		}
+		if usersRulesGroupItem.Container != nil {
+			usersRulesGroupItemMap["container"] = *usersRulesGroupItem.Container
+		}
+		if usersRulesGroupItem.Values != nil {
+			usersRulesGroupItemMap["values"] = flattenUsersRulesValue(usersRulesGroupItem.Values)
+		}
+
+		usersRulesGroupItemList = append(usersRulesGroupItemList, usersRulesGroupItemMap)
+	}
+
+	return usersRulesGroupItemList
+}
+
+func flattenUsersRulesCriteria(usersRulesCriteria *[]platformclientv2.Usersrulescriteria) []interface{} {
+	if usersRulesCriteria == nil {
+		return nil
+	}
+
+	usersRulesCriteriaList := make([]interface{}, 0)
+	for _, usersRulesCriteria := range *usersRulesCriteria {
+		usersRulesCriteriaMap := make(map[string]interface{})
+
+		if usersRulesCriteria.Id != nil {
+			usersRulesCriteriaMap["id"] = *usersRulesCriteria.Id
+		}
+		if usersRulesCriteria.Operator != nil {
+			usersRulesCriteriaMap["operator"] = *usersRulesCriteria.Operator
+		}
+		if usersRulesCriteria.Group != nil {
+			usersRulesCriteriaMap["group"] = flattenUsersRulesGroupItem(usersRulesCriteria.Group)
+		}
+
+		usersRulesCriteriaList = append(usersRulesCriteriaList, usersRulesCriteriaMap)
+	}
+
+	return usersRulesCriteriaList
+}
+
+func GenerateUsersRulesResource(
+	resourceLabel string,
+	name string,
+	description string,
+	ruleType string,
+	criteria []UsersRulesCriteriaStruct,
+) string {
+	return fmt.Sprintf(`resource "genesyscloud_users_rules" "%s" {
+		name = "%s"
+		description = "%s"
+		type = "%s"
+		%s
+	}
+	`, resourceLabel, name, description, ruleType, generateUsersRulesCriteria(criteria))
+}
+
+func generateUsersRulesCriteria(usersRulesCriteriaList []UsersRulesCriteriaStruct) string {
+	if usersRulesCriteriaList == nil {
+		return ""
+	}
+
+	var usersRulesCriteriaString string
+	for _, usersRulesCriteria := range usersRulesCriteriaList {
+		usersRulesCriteriaString += fmt.Sprintf(`
+			criteria {
+				id = "%s"
+				operator = "%s"
+				%s
+			}
+		`, usersRulesCriteria.Id, usersRulesCriteria.Operator, generateUsersRulesGroupItem(usersRulesCriteria.Group))
+	}
+
+	return usersRulesCriteriaString
+}
+
+func generateUsersRulesGroupItem(usersRulesGroupItems []UsersRulesGroupItemStruct) string {
+	if usersRulesGroupItems == nil {
+		return ""
+	}
+
+	var usersRulesGroupItemString string
+	for _, usersRulesGroupItem := range usersRulesGroupItems {
+		usersRulesGroupItemString += fmt.Sprintf(`
+			group {
+				id = "%s"
+				operator = "%s"
+				container = "%s"
+				%s
+			}
+		`, usersRulesGroupItem.Id, usersRulesGroupItem.Operator, usersRulesGroupItem.Container, generateUsersRulesValue(usersRulesGroupItem.Values))
+	}
+
+	return usersRulesGroupItemString
+}
+
+func generateUsersRulesValue(usersRulesValues []UsersRulesValueStruct) string {
+	if usersRulesValues == nil {
+		return ""
+	}
+
+	var usersRulesValueString string
+	for _, usersRulesValue := range usersRulesValues {
+		idsString := ""
+
+		for i, id := range usersRulesValue.Ids {
+			if i > 0 {
+				idsString += ", "
+			}
+
+			idsString += strconv.Quote(id)
+		}
+
+		usersRulesValueString += fmt.Sprintf(`
+		values {
+			context_id = "%s"
+			ids = [%s]
+		}
+		`, usersRulesValue.ContextId, idsString)
+	}
+
+	return usersRulesValueString
+}
