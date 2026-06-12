2291edd68f20532d77798c6f7a78ce268c4446ac
Suresh Periyappan <suresh.periyappan@genesys.com>
2026-06-12 18:25:46 +0530

    sdk version update

--- genesyscloud/tfexporter/resource_genesyscloud_tf_export_test.go
+++ genesyscloud/tfexporter/resource_genesyscloud_tf_export_test.go
@@ -37,7 +37,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/mypurecloud/platform-client-sdk-go/v188/platformclientv2"
+	"github.com/mypurecloud/platform-client-sdk-go/v191/platformclientv2"
 	"gonum.org/v1/gonum/graph/simple"
 	"gonum.org/v1/gonum/graph/topo"
 )
