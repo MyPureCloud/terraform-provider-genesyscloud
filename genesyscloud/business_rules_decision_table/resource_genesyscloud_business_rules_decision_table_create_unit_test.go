a453c669432838237569b2d28bb5a44a7f849d9f
Datta Sai Teja Tadi <datta_sai_teja.tadi@genesys.com>
2026-07-08 18:44:34 +0530

    fix: updated sdk dependecies from 189 to 193

--- genesyscloud/business_rules_decision_table/resource_genesyscloud_business_rules_decision_table_create_unit_test.go
+++ genesyscloud/business_rules_decision_table/resource_genesyscloud_business_rules_decision_table_create_unit_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/mypurecloud/platform-client-sdk-go/v188/platformclientv2"
+	"github.com/mypurecloud/platform-client-sdk-go/v193/platformclientv2"
 	"github.com/stretchr/testify/assert"
 )
 
