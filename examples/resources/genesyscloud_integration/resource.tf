1d9937d3b6043bc587e493ad9d5e37655ebd8a0e
vipulkumar.parmar <vipulkumar.parmar@genesys.com>
2026-07-28 11:43:01 -0400

    Adding hackathon specific resources

--- examples/resources/genesyscloud_integration/resource.tf
+++ examples/resources/genesyscloud_integration/resource.tf
@@ -51,3 +51,13 @@ resource "genesyscloud_integration" "example_gc_data_integration" {
     }
   }
 }
+
+resource "genesyscloud_integration" "hackathon_2026_integration" {
+  intended_state   = "ENABLED"
+  integration_type = "purecloud-data-actions"
+  config {
+    credentials = {
+      pureCloudOAuthClient = genesyscloud_integration_credential.example_purecloudoauth_credential.id
+    }
+  }
+}
--- examples/resources/genesyscloud_integration_action/resource.tf
+++ examples/resources/genesyscloud_integration_action/resource.tf
@@ -64,6 +64,194 @@ resource "genesyscloud_integration_action" "example_action" {
   }
 }
 
+resource "genesyscloud_integration_action" "hackathon_2026_first_action" {
+  name                   = "Hackathon 2026 First Action"
+  category               = "Genesys Cloud Data Action"
+  integration_id         = genesyscloud_integration.hackathon_2026_integration.id
+  secure                 = true
+  config_timeout_seconds = 20
+  contract_input = jsonencode({
+    "type" = "object",
+    "required" = [
+      "examplestr"
+    ],
+    "properties" = {
+      "examplestr" = {
+        "type" = "string"
+      },
+      "exampleint" = {
+        "type" = "integer"
+      },
+      "examplebool" = {
+        "type" = "boolean"
+      }
+    }
+  })
+  contract_output = jsonencode({
+    "type" = "object",
+    "required" = [
+      "status"
+    ],
+    "properties" = {
+      "status" = {
+        "type" = "string"
+      }
+      "outobj" = {
+        "type" = "object",
+        "properties" = {
+          "objstr" = {
+            "type" = "string"
+          }
+        }
+      }
+    }
+  })
+  config_request {
+    # Use '$${' to indicate a literal '${' in template strings. Otherwise Terraform will attempt to interpolate the string
+    # See https://www.terraform.io/docs/language/expressions/strings.html#escape-sequences
+    request_url_template = "https://www.example.com/health/check/services/$${input.service}"
+    request_type         = "GET"
+    request_template     = "$${input.rawRequest}"
+    headers = {
+      Cache-Control = "no-cache"
+    }
+  }
+  config_response {
+    translation_map = {
+      nameValue   = "$.Name"
+      buildNumber = "$.Build-Version"
+    }
+    translation_map_defaults = {
+      buildNumber = "UNKNOWN"
+    }
+    success_template = "{ \"name\": $${nameValue}, \"build\": $${buildNumber} }"
+  }
+}
+
+resource "genesyscloud_integration_action" "hackathon_2026_second_action" {
+  name                   = "Hackathon 2026 Second Action"
+  category               = "Genesys Cloud Data Action"
+  integration_id         = genesyscloud_integration.hackathon_2026_integration.id
+  secure                 = true
+  config_timeout_seconds = 20
+  contract_input = jsonencode({
+    "type" = "object",
+    "required" = [
+      "examplestr"
+    ],
+    "properties" = {
+      "examplestr" = {
+        "type" = "string"
+      },
+      "exampleint" = {
+        "type" = "integer"
+      },
+      "examplebool" = {
+        "type" = "boolean"
+      }
+    }
+  })
+  contract_output = jsonencode({
+    "type" = "object",
+    "required" = [
+      "status"
+    ],
+    "properties" = {
+      "status" = {
+        "type" = "string"
+      }
+      "outobj" = {
+        "type" = "object",
+        "properties" = {
+          "objstr" = {
+            "type" = "string"
+          }
+        }
+      }
+    }
+  })
+  config_request {
+    request_url_template = "https://www.example.com/health/check/services/$${input.service}"
+    request_type         = "GET"
+    request_template     = "$${input.rawRequest}"
+    headers = {
+      Cache-Control = "no-cache"
+    }
+  }
+  config_response {
+    translation_map = {
+      nameValue   = "$.Name"
+      buildNumber = "$.Build-Version"
+    }
+    translation_map_defaults = {
+      buildNumber = "UNKNOWN"
+    }
+    success_template = "{ \"name\": $${nameValue}, \"build\": $${buildNumber} }"
+  }
+}
+
+resource "genesyscloud_integration_action" "hackathon_2026_third_action" {
+  name                   = "Hackathon 2026 Third Action"
+  category               = "Genesys Cloud Data Action"
+  integration_id         = genesyscloud_integration.hackathon_2026_integration.id
+  secure                 = true
+  config_timeout_seconds = 20
+  contract_input = jsonencode({
+    "type" = "object",
+    "required" = [
+      "examplestr"
+    ],
+    "properties" = {
+      "examplestr" = {
+        "type" = "string"
+      },
+      "exampleint" = {
+        "type" = "integer"
+      },
+      "examplebool" = {
+        "type" = "boolean"
+      }
+    }
+  })
+  contract_output = jsonencode({
+    "type" = "object",
+    "required" = [
+      "status"
+    ],
+    "properties" = {
+      "status" = {
+        "type" = "string"
+      }
+      "outobj" = {
+        "type" = "object",
+        "properties" = {
+          "objstr" = {
+            "type" = "string"
+          }
+        }
+      }
+    }
+  })
+  config_request {
+    request_url_template = "https://www.example.com/health/check/services/$${input.service}"
+    request_type         = "GET"
+    request_template     = "$${input.rawRequest}"
+    headers = {
+      Cache-Control = "no-cache"
+    }
+  }
+  config_response {
+    translation_map = {
+      nameValue   = "$.Name"
+      buildNumber = "$.Build-Version"
+    }
+    translation_map_defaults = {
+      buildNumber = "UNKNOWN"
+    }
+    success_template = "{ \"name\": $${nameValue}, \"build\": $${buildNumber} }"
+  }
+}
+
 # Example with function configuration
 # Note: function_config is only required for function data actions (when category = "Genesys Cloud Data Action")
 # For regular integration actions, this section can be omitted
