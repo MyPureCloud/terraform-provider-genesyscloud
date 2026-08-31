47b5ee2a9b89b282e69029cfc899fc65883f5809
Jiongxu <jiongxu.hou@genesys.com>
2026-08-31 17:42:25 +0100

    rm local test

--- local-test/stress-test-8000-rows/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "genesyscloud" {
-  oauthclient_id     = "8043f756-2052-4653-934d-9bdb5120d039"
-  oauthclient_secret = "G98C__50SIedka-sSzf4tt52aUMs93v9N3boJHLKNB-FsG5nglrnAAnwDLIs"
-  aws_region         = "dca"
-}
