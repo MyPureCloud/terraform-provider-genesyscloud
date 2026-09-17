689c23056863f9ad87657664c4b47a862c6fd58c
Viktoria Szathmary <viktoria.szathmary@genesys.com>
2026-09-17 13:53:29 +0200

    RELATE-25226: examples and docs created

---
+++ examples/resources/genesyscloud_routing_email_route_identity_resolution/apis.md
@@ -0,0 +1,6 @@
+<!-- sources
+genesyscloud/routing_email_route_identity_resolution/genesyscloud_routing_email_route_identity_resolution_proxy.go
+-->
+* [GET /api/v2/routing/email/domains/{domainName}/routes/{routeId}](https://developer.genesys.cloud/devapps/api-explorer#get-api-v2-routing-email-domains--domainName--routes--routeId-)
+* [GET /api/v2/routing/email/domains/{domainName}/routes/{routeId}/identityresolution](https://developer.genesys.cloud/devapps/api-explorer#get-api-v2-routing-email-domains--domainName--routes--routeId--identityresolution)
+* [PUT /api/v2/routing/email/domains/{domainName}/routes/{routeId}/identityresolution](https://developer.genesys.cloud/devapps/api-explorer#put-api-v2-routing-email-domains--domainName--routes--routeId--identityresolution)
--- genesyscloud/provider_registrar/provider_registrar.go
+++ genesyscloud/provider_registrar/provider_registrar.go
@@ -117,6 +117,7 @@ import (
 	responsemanagementResponseasset "github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/responsemanagement_responseasset"
 	routingEmailDomain "github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/routing_email_domain"
 	routingEmailRoute "github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/routing_email_route"
+	routingEmailRouteIdentityResolution "github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/routing_email_route_identity_resolution"
 	routingLanguage "github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/routing_language"
 	routingQueue "github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/routing_queue"
 	routingQueueConditionalGroupActivation "github.com/mypurecloud/terraform-provider-genesyscloud/genesyscloud/routing_queue_conditional_group_activation"
@@ -339,6 +340,7 @@ func registerResources() {
 	responsemanagementResponseasset.SetRegistrar(regInstance)              //Registering responsemanagement response asset
 	respmanagementLibrary.SetRegistrar(regInstance)                        //Registering responsemanagement library
 	routingEmailRoute.SetRegistrar(regInstance)                            //Registering routing email route
+	routingEmailRouteIdentityResolution.SetRegistrar(regInstance)          //Registering routing email route identity resolution
 	did.SetRegistrar(regInstance)                                          //Registering telephony did
 	didPool.SetRegistrar(regInstance)                                      //Registering telephony did pools
 	archIvr.SetRegistrar(regInstance)                                      //Registering architect ivr
