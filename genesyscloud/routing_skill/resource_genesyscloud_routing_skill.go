ce74f049865d059d94b8b01f009600946089e936
Eamonn Clancy <eclancy404@gmail.com>
2026-09-18 10:19:53 +0100

    PLUS-2895: Unhide Division ID (#2562)
    * update platform sdk
    
    * revert to create routing skill with division id in create body
    
    * bump platform sdk
    
    ---------
    
    Co-authored-by: Eamonn Clancy <eamonn.clancy@genesys.com>

--- genesyscloud/routing_skill/resource_genesyscloud_routing_skill.go
+++ genesyscloud/routing_skill/resource_genesyscloud_routing_skill.go
@@ -47,6 +47,11 @@ func createRoutingSkill(ctx context.Context, d *schema.ResourceData, meta interf
 	name := d.Get("name").(string)
 	createRoutingSkill.Name = &name
 
+	divisionId := d.Get("division_id").(string)
+	if divisionId != "" {
+		createRoutingSkill.DivisionId = &divisionId
+	}
+
 	log.Printf("Creating skill %s", name)
 	skill, resp, err := proxy.createRoutingSkill(ctx, &createRoutingSkill)
 	if err != nil {
@@ -55,20 +60,6 @@ func createRoutingSkill(ctx context.Context, d *schema.ResourceData, meta interf
 
 	d.SetId(*skill.Id)
 	log.Printf("Created skill %s %s", name, *skill.Id)
-
-	// If a division_id is specified, move the skill to that division via PATCH
-	divisionId := d.Get("division_id").(string)
-	if divisionId != "" {
-		var updateReq platformclientv2.Updateskilldivisionrequest
-		updateReq.DivisionId = &divisionId
-
-		log.Printf("Moving skill %s to division %s", *skill.Id, divisionId)
-		_, resp, err := proxy.updateRoutingSkill(ctx, *skill.Id, &updateReq)
-		if err != nil {
-			return util.BuildAPIDiagnosticError(ResourceType, fmt.Sprintf("Failed to move skill %s to division %s error: %s", name, divisionId, err), resp)
-		}
-	}
-
 	return readRoutingSkill(ctx, d, meta)
 }
 
